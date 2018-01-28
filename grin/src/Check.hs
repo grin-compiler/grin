0c1d2700660c1c46147a1ab556bd8b5be275a0a8
Andor Penzes <andor.penzes@gmail.com>
2018-01-28 02:34:22 +0100

    Add Allowed BindStore check

--- grin/src/Check.hs
+++ grin/src/Check.hs
@@ -1,12 +1,14 @@
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE LambdaCase, BangPatterns #-}
 module Check where
 
+import Control.Arrow
 import Data.Functor.Foldable
 import Data.Monoid
 import Grin
 
 import Data.Map.Strict as Map
-import Data.Set        as Set
+import Data.Set        as Set hiding (fold)
+import qualified Data.Foldable as Foldable
 
 
 data Check
@@ -15,7 +17,14 @@ data Check
   | OnlyBasicValuesInCases
   | OnlyTagsInAlts
   | OnlyUniqueNames
-  deriving (Eq, Show)
+  | AllowedBindStoreValues
+  deriving (Enum, Eq, Show)
+
+allChecks :: Exp -> [(Check, Bool)]
+allChecks = checks . enumFrom $ toEnum 0
+
+checks :: [Check] -> Exp -> [(Check, Bool)]
+checks cs e = zipWith (\c e -> (c, check c e)) cs (repeat e)
 
 check :: Check -> Exp -> Bool
 check = \case
@@ -24,11 +33,21 @@ check = \case
   OnlyBasicValuesInCases -> getAll . valuesInCases (All . isBasicValue)
   OnlyTagsInAlts         -> getAll . patsInAlts    (All . isBasicCPat)
   OnlyUniqueNames        -> Prelude.null . nonUniqueNames
+  AllowedBindStoreValues -> allowedBindStoreValues
+
+allowedBindStoreValues :: Exp -> Bool
+allowedBindStoreValues = getAll . bindStoreValues (\case
+  Loc _ -> All True
+  _     -> All False)
+
+bindStoreValues :: Monoid m => (Val -> m) -> Exp -> m
+bindStoreValues f = para $ \case
+  EBindF ((SStore _), l) val (_, r) -> mconcat [f val, l, r]
+  rest                              -> Foldable.foldMap snd rest
 
 -- | Names introduced by fetch def and patterns.
 definedNames :: Monoid m => (Name -> m) -> ExpF m -> m
 definedNames f = \case
-  ProgramF names -> mconcat names
   DefF defName params names -> mconcat
     [ f defName
     , mconcat (f <$> params)
@@ -40,40 +59,30 @@ definedNames f = \case
     , restNames
     , foldNames f lpat -- Names in lpat are always new names
     ]
-  ECaseF    val names -> mconcat names
   -- Simple Expr
-  SAppF     name valNames -> mempty -- Application does not introduce names
-  SReturnF  valName -> mempty
-  SStoreF   valName -> mempty
   SFetchIF  name pos -> f name
-  SUpdateF  name val -> mempty
   SBlockF   names    -> names
   -- Alt
   AltF cpat names -> mconcat
     [ names
     , foldNames f cpat -- Names in cpat are always new names
     ]
+  rest -> Foldable.fold rest
 
 -- | Names that are used in the entire program.
 usedNames :: Monoid m => (Name -> m) -> ExpF m -> m
 usedNames f = \case
-  ProgramF  names -> mconcat names
-  DefF      defName params names -> names
   -- Exp
   EBindF    simpleNames lpat restNames -> mconcat
     [ simpleNames
     , restNames
     ]
-  ECaseF    val names -> mconcat names
   -- Simple Expr
   SAppF     name valNames -> mconcat [f name, mconcat (foldNames f <$> valNames)]
   SReturnF  valName -> foldNames f valName
   SStoreF   valName -> foldNames f valName
-  SFetchIF  name pos -> mempty
   SUpdateF  name val -> f name
-  SBlockF   names    -> names
-  -- Alt
-  AltF cpat names -> names
+  rest -> Foldable.fold rest
 
 nonUniqueNames :: Exp -> [Name]
 nonUniqueNames = Map.keys . Map.filter ((> 1) . getSum) . cata (definedNames insert) where
@@ -86,50 +95,32 @@ nonDefinedNames e = Set.toList $ Set.difference
 
 storedValues :: Monoid m => (Val -> m) -> ExpF m -> m
 storedValues f = \case
-  ProgramF vals      -> mconcat vals
-  DefF _ _ val       -> val
-  EBindF val1 _ val2 -> val1 <> val2
-  SStoreF val        -> f val
-  SBlockF val        -> val
-  AltF _ val         -> val
-  _                  -> mempty
+  SStoreF val -> f val
+  rest        -> Foldable.fold rest
 
 storedConstants :: Exp -> [Val]
 storedConstants = cata (storedValues (\v -> [v | isConstant v]))
 
 valuesInCases :: (Monoid m) => (Val -> m) -> Exp -> m
 valuesInCases f = cata $ \case
-  ProgramF vals -> mconcat vals
-  DefF _ _ val  -> val
   ECaseF val alts -> mconcat (f val:alts)
-  EBindF val1 _ val2 -> val1 <> val2
-  SBlockF val -> val
-  AltF _ val -> val
-  _ -> mempty
+  rest            -> Foldable.fold rest
 
 patsInAlts :: (Monoid m) => (CPat -> m) -> Exp -> m
 patsInAlts f = cata $ \case
-  ProgramF vals -> mconcat vals
-  DefF _ _ val  -> val
-  ECaseF val alts -> mconcat alts
-  EBindF val1 _ val2 -> val1 <> val2
-  SBlockF val -> val
   AltF pat val -> (f pat <> val)
-  _ -> mempty
+  rest         -> Foldable.fold rest
 
 programSize :: Exp -> Int
 programSize = cata $ \case
-  ProgramF  ds    -> sum ds
-  DefF      _ _ a -> 1 + a
-  -- Exp
-  EBindF    a _ b -> sum [1,a,b]
-  ECaseF    _ as  -> sum (1:as)
-  -- Simple Expr
-  SAppF     _ _   -> 1
-  SReturnF  _     -> 1
-  SStoreF   _     -> 1
-  SFetchIF  _ _   -> 1
-  SUpdateF  _ _   -> 1
-  SBlockF   a     -> 1 + a
-  -- Alt
-  AltF _ a        -> 1 + a
+  ProgramF  ds      -> sum ds
+  DefF      _ _ !a  -> 1 + a
+  EBindF    !a _ !b -> sum [1,a,b]
+  ECaseF    _ !as   -> sum (1:as)
+  SAppF     _ _     -> 1
+  SReturnF  _       -> 1
+  SStoreF   _       -> 1
+  SFetchIF  _ _     -> 1
+  SUpdateF  _ _     -> 1
+  SBlockF   !a      -> 1 + a
+  AltF _ !a         -> 1 + a
