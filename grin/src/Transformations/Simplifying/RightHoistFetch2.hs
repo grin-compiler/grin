3d13e8855284e27dea9b6204e1a26d6a68e65b18
Csaba Hruska <csaba.hruska@gmail.com>
2018-02-11 20:02:45 +0100

    work on new right hoist fetch

--- grin/src/Transformations/Simplifying/RightHoistFetch2.hs
+++ grin/src/Transformations/Simplifying/RightHoistFetch2.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE LambdaCase, TupleSections, TypeApplications, RecordWildCards, DeriveFunctor #-}
+{-# LANGUAGE LambdaCase, TupleSections, TypeApplications, RecordWildCards, DeriveFunctor, TemplateHaskell #-}
 module Transformations.Simplifying.RightHoistFetch2 where
 
 import Debug.Trace (traceShowId, trace)
@@ -11,6 +11,8 @@ import Data.Set (Set)
 import qualified Data.Set as Set
 import Data.Functor.Foldable as Foldable
 import qualified Data.Foldable
+
+import Lens.Micro.Platform
 
 import Grin
 
@@ -76,7 +78,7 @@ collectFetchVars = fmap (fmap Set.toList . monoidMap) . globalMap . para collect
 
       ECaseF val alts   -> mconcat $ useNameVal val : zipWith (prefixStep . SAlt) [0..] alts
 
-      -- the latter can be handled in generic way
+      -- the rest can be handled in generic way
       SAppF _name args  -> mconcat $ map useNameVal args
       SReturnF val      -> useNameVal val
       SStoreF val       -> useNameVal val
@@ -113,10 +115,11 @@ prefixStep step rhid@RHF{..} = rhid
           
     dead = not $ any (isPrefix scope) scopes ; remove
 -}
-rightHoistFetch :: Exp -> Exp
-rightHoistFetch e = trace (ppShow globalFetchMap) $ ana builder ([], mempty, e)
+_rightHoistFetch :: Exp -> Exp
+_rightHoistFetch e = trace (printf "old:\n%s\nnew:\n%s" (ppShow globalFetchMap) (ppShow globalFetchMap2)) $ ana builder ([], mempty, e)
   where
     globalFetchMap = collectFetchVars e
+    globalFetchMap2 = collectFetchVars2 e
 
     builder :: ([Int], FetchVarMap, Exp) -> ExpF ([Int], FetchVarMap, Exp)
     builder (path, liveVars, exp) = case exp of
@@ -145,3 +148,110 @@ rightHoistFetch e = trace (ppShow globalFetchMap) $ ana builder ([], mempty, e)
     - mapVarNames, foldVarNames for Val and Exp (non recursive)
     - 
 -}
+
+------------------
+
+{-
+  NAMING:
+    caseVar <- fetch fetchVar[0]
+    itemVar <- fetch fetchVar[1] ; index > 0
+    case caseVar of
+
+  New and simplified algorithm
+    - collect caseVar and fetchVar
+    - move every non tagged indexed fetch under the corresponding case and tag them according the alternative
+    - use the tag info to determine the tag arity and move only the relevant fetch operation
+
+  RESTRICTION:
+    - values come from untagged indexed fetches must not be used before the corresponding case
+      otherwise it is ERROR or the operations have to be MOVED under the case along with the fetches
+
+  HINT:
+    - do not care about name aliases, that is responsibility of other transformations
+    - ignore the variable liveness, the dead variable eliminiation will get rid of it
+-}
+data RHF2
+  = RHF2
+  { fetchVars   :: Map Name Name -- caseVar -> fetchVar ; MANY - ONE
+  , caseVars    :: Set Name
+  , globalMap2  :: Map Name (Set Name) -- def -> Set fetchVar
+  }
+  deriving Show
+
+--concat <$> mapM makeLenses [''NodeSet, ''Value, ]
+
+instance Monoid RHF2 where
+  mempty = RHF2 mempty mempty mempty
+  mappend (RHF2 a1 b1 c1) (RHF2 a2 b2 c2) = RHF2 (a1 `mappend` a2) (b1 `mappend` b2) (c1 `mappend` c2)
+
+{-
+  QUESTION:
+    What about fetch var dependency for non tag vars?
+  ANSWER:
+    Hoist every non tag fetch under the corresponding case because every fetch must be tagged.
+    This is a codegen requirement.
+-}
+
+--collectFetchVars2 :: Exp -> Map Name (Set Name)
+collectFetchVars2 :: Exp -> Set Name
+collectFetchVars2 = cull . para collect where
+  collect :: ExpF (Exp, RHF2) -> RHF2
+  collect = \case
+    EBindF (SFetchI fetchVar (Just 0), left) (Var caseVar) (_, right) -> mconcat [right, addFetchVar caseVar fetchVar]
+
+    expRHF -> case fmap snd expRHF of
+      ECaseF (Var caseVar) alts -> mconcat $ addCaseVar caseVar : alts
+{-
+      DefF name _args RHF2{..} -> addGlobal name fetchVarSet
+                where -- keep only fetch variables that have corresponding case
+                      fetchVarSet = Set.fromList . Map.elems $ Map.filterWithKey (\n _ -> Set.member n caseVars) fetchVars
+-}
+      e -> Data.Foldable.fold e
+
+  addCaseVar caseVar = RHF2 mempty (Set.singleton caseVar) mempty
+  addFetchVar caseVar fetchVar = RHF2 (Map.singleton caseVar fetchVar) mempty mempty
+  --addGlobal name fetchVarSet = RHF2 mempty mempty (Map.singleton name fetchVarSet)
+
+  cull RHF2{..} = Set.fromList . Map.elems $ Map.filterWithKey (\n _ -> Set.member n caseVars) fetchVars
+
+{-
+  FIRST VERSION:
+    - no fetch var path filtering
+    - move all fetches under the case, delete them when moved
+    - substitute names
+-}
+
+data RHF3
+  = RHF3
+  { _path       :: [Int]
+  , _hoistMap   :: Map Name [(Name, Int)] -- fetchVar -> [(itemVar, fetchIndex)]
+  , _caseMap    :: Map Name Name          -- caseVar  -> fetchVar
+  }
+  deriving Show
+
+concat <$> mapM makeLenses [''RHF3]
+
+emptyRHF3 = RHF3 mempty mempty mempty
+
+rightHoistFetch :: Exp -> Exp
+rightHoistFetch e = trace (printf "fetch vars:\n%s" (ppShow globalFetchMap)) $ ana builder ([], mempty, e)
+  where
+    globalFetchMap = collectFetchVars2 e
+
+    builder :: ([Int], Map Name [(Name, Int)], Exp) -> ExpF ([Int], Map Name [(Name, Int)], Exp)
+    builder (path, hoistMap, exp) = case exp of
+      --Def name args body -> DefF name args ([], globalFetchMap Map.! name, mempty, body)
+
+      -- Remove original fetch
+      -- Must emit some code, apo does not have a skip command
+      -- TODO: locVarName could be substituted, store fetch command here
+      EBind fetch@(SFetchI fetchVar (Just idx)) lpat@(Var caseVar) rightExp
+          | Set.member fetchVar globalFetchMap -> case idx of
+              -- TODO: save caseVar for case recognition
+              0 -> EBindF (path, hoistMap, fetch) lpat (path, hoistMap, rightExp)
+              _ -> EBindF (path, hoistMap, SReturn Unit) Unit (path, hoistMap, rightExp)
+
+      -- filter out divergent paths
+      ECase val alts -> ECaseF val [(path ++ [i], hoistMap, alt) | (i,alt) <- zip [0..] alts]
+
+      _ -> (path,hoistMap,) <$> project exp
