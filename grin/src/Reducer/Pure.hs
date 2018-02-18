e9b9981aab1022022a8979c73343921fcf969791
Csaba Hruska <csaba.hruska@gmail.com>
2018-02-18 13:35:53 +0100

    fix pure reducer error pretty printer

--- grin/src/Reducer/Pure.hs
+++ grin/src/Reducer/Pure.hs
@@ -16,6 +16,9 @@ import Reducer.PrimOps
 import Grin
 import Pretty
 
+prettyDebug :: Pretty a => a -> String
+prettyDebug = show . plain . pretty
+
 -- models computer memory
 data StoreMap
   = StoreMap
@@ -33,7 +36,7 @@ bindPatMany :: Env -> [Val] -> [LPat] -> Env
 bindPatMany env [] [] = env
 bindPatMany env (val : vals) (lpat : lpats) = bindPatMany (bindPat env val lpat) vals lpats
 bindPatMany env [] (lpat : lpats) = bindPatMany (bindPat env Undefined lpat) [] lpats
-bindPatMany _ vals lpats = error $ printf "bindPatMany - pattern mismatch: %s %s" (show . pretty $ vals) (show . pretty $ lpats)
+bindPatMany _ vals lpats = error $ printf "bindPatMany - pattern mismatch: %s %s" (prettyDebug vals) (prettyDebug lpats)
 
 bindPat :: Env -> Val -> LPat -> Env
 bindPat env val lpat = case lpat of
@@ -41,13 +44,13 @@ bindPat env val lpat = case lpat of
   ConstTagNode ptag pargs   | ConstTagNode vtag vargs <- val, ptag == vtag -> bindPatMany env vargs pargs
   VarTagNode varname pargs  | ConstTagNode vtag vargs <- val               -> bindPatMany (Map.insert varname (ValTag vtag) env) vargs pargs
   Unit -> env
-  _ -> error $ printf "bindPat - pattern mismatch %s %s" (show . pretty $ val) (show . pretty $ lpat)
+  _ -> error $ printf "bindPat - pattern mismatch %s %s" (prettyDebug val) (prettyDebug lpat)
 
 lookupEnv :: Name -> Env -> Val
 lookupEnv n env = Map.findWithDefault (error $ printf "missing variable: %s" n) n env
 
 lookupStore :: Int -> StoreMap -> Val
-lookupStore i s = IntMap.findWithDefault (error $ printf "missing location: %s" (show i)) i $ storeMap s
+lookupStore i s = IntMap.findWithDefault (error $ printf "missing location: %d" i) i $ storeMap s
 
 evalVal :: Env -> Val -> Val
 evalVal env = \case
@@ -58,11 +61,11 @@ evalVal env = \case
                   -- NOTE: must be impossible (I guess)
                   -- Var n     -> VarTagNode n $ map (evalVal env) a
                   ValTag t  -> ConstTagNode t $ map (evalVal env) a
-                  x -> error $ printf "evalVal - invalid VarTagNode tag: %s" (show $ pretty x)
+                  x -> error $ printf "evalVal - invalid VarTagNode tag: %s" (prettyDebug x)
   v@ValTag{}  -> v
   v@Unit      -> v
   v@Loc{}     -> v
-  x -> error $ printf "evalVal: %s" (show $ pretty x)
+  x -> error $ printf "evalVal: %s" (prettyDebug x)
 
 evalSimpleExp :: Env -> SimpleExp -> GrinM Val
 evalSimpleExp env = \case
@@ -70,7 +73,7 @@ evalSimpleExp env = \case
               let args = map (evalVal env) a
                   go a [] [] = a
                   go a (x:xs) (y:ys) = go (Map.insert x y a) xs ys
-                  go _ x y = error $ printf "invalid pattern for function: %s %s %s" n (show $ pretty x) (show $ pretty y)
+                  go _ x y = error $ printf "invalid pattern for function: %s %s %s" n (prettyDebug x) (prettyDebug y)
               if isPrimName n
                 then evalPrimOp n args
                 else do
@@ -84,7 +87,7 @@ evalSimpleExp env = \case
               return $ Loc l
   SFetchI n index -> case lookupEnv n env of
               Loc l -> gets $ (selectNodeItem index . lookupStore l)
-              x -> error $ printf "evalSimpleExp - Fetch expected location, got: %s" (show $ pretty x)
+              x -> error $ printf "evalSimpleExp - Fetch expected location, got: %s" (prettyDebug x)
 --  | FetchI  Name Int -- fetch node component
   SUpdate n v -> do
               let v' = evalVal env v
@@ -92,23 +95,23 @@ evalSimpleExp env = \case
                 Loc l -> get >>= \(StoreMap m _) -> case IntMap.member l m of
                             False -> error $ printf "evalSimpleExp - Update unknown location: %d" l
                             True  -> modify' (\(StoreMap m s) -> StoreMap (IntMap.insert l v' m) s) >> return Unit
-                x -> error $ printf "evalSimpleExp - Update expected location, got: %s" (show $ pretty x)
+                x -> error $ printf "evalSimpleExp - Update expected location, got: %s" (prettyDebug x)
   SBlock a -> evalExp env a
-  x -> error $ printf "evalSimpleExp: " (show $ pretty x)
+  x -> error $ printf "evalSimpleExp: %s" (prettyDebug x)
 
 evalExp :: Env -> Exp -> GrinM Val
 evalExp env = \case
   EBind op pat exp -> evalSimpleExp env op >>= \v -> evalExp (bindPat env v pat) exp
   ECase v alts -> case evalVal env v of
     ConstTagNode t l ->
-                   let (vars,exp) = head $ [(b,exp) | Alt (NodePat a b) exp <- alts, a == t] ++ error (printf "evalExp - missing Case Node alternative for: %s" (show $ pretty t))
+                   let (vars,exp) = head $ [(b,exp) | Alt (NodePat a b) exp <- alts, a == t] ++ error (printf "evalExp - missing Case Node alternative for: %s" (prettyDebug t))
                        go a [] [] = a
                        go a (x:xs) (y:ys) = go (Map.insert x y a) xs ys
-                       go _ x y = error $ printf "invalid pattern and constructor: %s %s %s" (show $ pretty t) (show $ pretty x) (show $ pretty y)
+                       go _ x y = error $ printf "invalid pattern and constructor: %s %s %s" (prettyDebug t) (prettyDebug x) (prettyDebug y)
                    in  evalExp (go env vars l) exp
-    ValTag t    -> evalExp env $ head $ [exp | Alt (TagPat a) exp <- alts, a == t] ++ error (printf "evalExp - missing Case Tag alternative for: %s" (show $ pretty t))
-    Lit l       -> evalExp env $ head $ [exp | Alt (LitPat a) exp <- alts, a == l] ++ error (printf "evalExp - missing Case Lit alternative for: %s" (show $ pretty l))
-    x -> error $ printf "evalExp - invalid Case dispatch value: %s" (show $ pretty x)
+    ValTag t    -> evalExp env $ head $ [exp | Alt (TagPat a) exp <- alts, a == t] ++ error (printf "evalExp - missing Case Tag alternative for: %s" (prettyDebug t))
+    Lit l       -> evalExp env $ head $ [exp | Alt (LitPat a) exp <- alts, a == l] ++ error (printf "evalExp - missing Case Lit alternative for: %s" (prettyDebug l))
+    x -> error $ printf "evalExp - invalid Case dispatch value: %s" (prettyDebug x)
   exp -> evalSimpleExp env exp
 
 reduceFun :: [Def] -> Name -> Val
