52aa3d1b7ab95e826b54aa644fbe4844afedd9ff
Csaba Hruska <csaba.hruska@gmail.com>
2018-02-23 15:01:16 +0100

    initial version of the dependent GRIN type system

--- grin/src/TypeEnv.hs
+++ grin/src/TypeEnv.hs
@@ -23,6 +23,12 @@ type NodeSet = Map Tag (Vector SimpleType)
 data Type
   = T_SimpleType  {_simpleType  :: SimpleType}
   | T_NodeSet     {_nodeSet     :: NodeSet}
+
+  -- dependent type constructions to describe deconstructed nodes
+  | T_Tag         {_tagDomain   :: NodeSet}   -- tag with it's corresponding node set domain
+  | T_Item        {_tagVariable :: Name       -- tag variable name that holds the node tag on which the item type depends
+                  ,_itemIndex   :: Int        -- item index in the node
+                  }
   deriving (Eq, Ord, Show)
 
 data TypeEnv
