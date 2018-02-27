8958a1530b6fae69659a377d3fc707d111bda99e
Csaba Hruska <csaba.hruska@gmail.com>
2018-02-27 14:53:49 +0100

    implement evaluated case elimination

---
+++ grin/src/Transformations/Optimising/EvaluatedCaseElimination.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE LambdaCase, TupleSections #-}
+module Transformations.Optimising.EvaluatedCaseElimination where
+
+import Data.Functor.Foldable as Foldable
+import Grin
+
+evaluatedCaseElimination :: Exp -> Exp
+evaluatedCaseElimination = ana builder where
+  builder :: Exp -> ExpF Exp
+  builder = \case
+    ECase val alts | all (altBodyEQ $ SReturn val) alts -> SReturnF val
+    exp -> project exp
+
+  altBodyEQ :: Exp -> Alt -> Bool
+  altBodyEQ exp (Alt _cpat body) = exp == body
