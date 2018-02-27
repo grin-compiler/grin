3c3280d9849471b0167ce0bfc8bf709cd0ff612d
Csaba Hruska <csaba.hruska@gmail.com>
2018-02-27 16:12:31 +0100

    implement update elimination

---
+++ grin/src/Transformations/Optimising/UpdateElimination.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE LambdaCase, TupleSections #-}
+module Transformations.Optimising.UpdateElimination where
+
+import Data.Functor.Foldable as Foldable
+import Grin
+
+updateElimination :: Exp -> Exp
+updateElimination = ana builder where
+  builder :: Exp -> ExpF Exp
+  builder = \case
+    EBind   (SFetch  fetchLoc) fetchVal
+     (EBind (SUpdate updateLoc updateVal) Unit rightExp)
+      | fetchLoc == updateLoc &&
+        fetchVal == updateVal
+        -> EBindF (SFetch fetchLoc) fetchVal rightExp
+
+    exp -> project exp
