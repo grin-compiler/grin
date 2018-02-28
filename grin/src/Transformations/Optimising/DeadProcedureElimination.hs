7917d822ca7c4c7f24ca2e9ee08baf32ec0170ac
Csaba Hruska <csaba.hruska@gmail.com>
2018-02-28 20:37:41 +0100

    implement dead procedure elimination

---
+++ grin/src/Transformations/Optimising/DeadProcedureElimination.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE LambdaCase, TupleSections #-}
+module Transformations.Optimising.DeadProcedureElimination where
+
+import Text.Printf
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.Functor.Foldable as Foldable
+import qualified Data.Foldable
+import Grin
+
+deadProcedureElimination :: Program -> Program
+deadProcedureElimination (Program defs) = Program [def | def@(Def name _ _) <- defs, Set.member name liveDefs] where
+  defMap :: Map Name Def
+  defMap = Map.fromList [(name, def) | def@(Def name _ _) <- defs]
+
+  lookupDef :: Name -> Def
+  lookupDef name = case Map.lookup name defMap of
+    Nothing -> error $ printf "unknown function: %s" name
+    Just a  -> a
+
+  liveDefs :: Set Name
+  liveDefs = fst $ until (\(live, visited) -> live == visited) visit (Set.singleton "grinMain", mempty)
+
+  visit :: (Set Name, Set Name) -> (Set Name, Set Name)
+  visit (live, visited) = (mappend visited seen, live) where
+    seen = foldMap (cata collect . lookupDef) $ Set.difference live visited
+
+  collect :: ExpF (Set Name) -> Set Name
+  collect = \case
+    SAppF name _ | not (isPrimName name) -> Set.singleton name
+    exp -> Data.Foldable.fold exp
