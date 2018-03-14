2c77ed7fa8b30fd073e3c358ee5ef4c56436d608
Andor Penzes <andor.penzes@gmail.com>
2018-03-14 22:54:59 +0100

    More readable test for CaseCopyPropagation.

--- grin/test/Transformations/Optimising/CaseCopyPropagationSpec.hs
+++ grin/test/Transformations/Optimising/CaseCopyPropagationSpec.hs
@@ -5,7 +5,6 @@ module Transformations.Optimising.CaseCopyPropagationSpec where
 import Transformations.Optimising.CaseCopyPropagation
 
 import Test.Hspec
-import Free hiding (def)
 import Grin
 import GrinTH
 import Test
@@ -17,184 +16,185 @@ spec :: Spec
 spec = do
 
   it "Example from Figure 4.26" $ do
-    before <- buildExpM $
-      "m0" <=: store @Int 3 $
-      "u" <=: block (switch "v"
-        [ ("foo" @@: ["a"],  "y'" <=: app "foo" ["a"] $
-                             unit @Val ("Int" @: ["y'"]))
-        , ("bar" @@: ["b"],  "z'" <=: app "bar" ["b"] $
-                             unit @Val ("Int" @: ["z'"]))
-        , ("Int" @: ["x'"], unit @Val ("Int" @: ["x'"]))
-        ]) $
-      unit @Var "m0"
-
-    after  <- buildExpM $
-      "m0" <=: store @Int 3 $
-      "u" <=: (block $
-                "v'" <=: (block $ switch "v"
-                  [ ("foo" @@: ["a"],  "y'" <=: app "foo" ["a"] $
-                                       unit @Var "y'")
-                  , ("bar" @@: ["b"],  "z'" <=: app "bar" ["b"] $
-                                       unit @Var "z'")
-                  , ("Int" @: ["x'"], unit @Var "x'")
-                  ]) $
-                unit @Val ("Int" @: ["v'"])) $
-      unit @Var "m0"
-
+    let before = [expr|
+            m0 <- store 3
+            u  <- do
+              case v of
+                (Ffoo a)  -> y' <- foo a
+                             pure (CInt y')
+                (Fbar b)  -> z' <- bar b
+                             pure (CInt z')
+                (CInt x') -> pure (CInt x')
+            pure m0
+          |]
+    let after = [expr|
+            m0 <- store 3
+            u  <- do
+              v' <- do
+                case v of
+                  (Ffoo a)  -> y' <- foo a
+                               pure y'
+                  (Fbar b)  -> z' <- bar b
+                               pure z'
+                  (CInt x') -> pure x'
+              pure (CInt v')
+            pure m0
+          |]
     caseCopyPropagation before `sameAs` after
 
   it "One node has no Int tagged value" $ do
-    before <- buildExpM $
-      "m0" <=: store @Int 3 $
-      "u" <=: block (switch "v"
-        [ ("foo" @@: ["a"],  "y'" <=: app "foo" ["a"] $
-                             unit @Val ("Int" @: ["y'"]))
-        , ("bar" @@: ["b"],  "z'" <=: app "bar" ["b"] $
-                             unit @Val ("Float" @: ["z'"]))
-        , ("Int" @: ["x'"], unit @Val ("Int" @: ["x'"]))
-        ]) $
-      unit @Var "m0"
+    let before = [expr|
+            m0 <- store 3
+            u  <- do
+              case v of
+                (Ffoo a)  -> y' <- foo a
+                             pure (CInt y')
+                (Fbar b)  -> z' <- bar b
+                             pure (CFloat z')
+                (CInt x') -> pure (CInt x')
+            pure m0
+          |]
 
-    after <- buildExpM $
-      "m0" <=: store @Int 3 $
-      "u" <=: block (switch "v"
-        [ ("foo" @@: ["a"],  "y'" <=: app "foo" ["a"] $
-                             unit @Val ("Int" @: ["y'"]))
-        , ("bar" @@: ["b"],  "z'" <=: app "bar" ["b"] $
-                             unit @Val ("Float" @: ["z'"]))
-        , ("Int" @: ["x'"], unit @Val ("Int" @: ["x'"]))
-        ]) $
-      unit @Var "m0"
-
+    let after = [expr|
+            m0 <- store 3
+            u  <- do
+              case v of
+                (Ffoo a)  -> y' <- foo a
+                             pure (CInt y')
+                (Fbar b)  -> z' <- bar b
+                             pure (CFloat z')
+                (CInt x') -> pure (CInt x')
+            pure m0
+          |]
     caseCopyPropagation before `sameAs` after
 
   it "Embedded good case" $ do
-    before <- buildExpM $
-      "m0" <=: store @Int 3 $
-      "u" <=: block (switch "v"
-        [ ("foo" @@: ["a"],  "y'" <=: app "foo" ["a"] $
-                             unit @Val ("Int" @: ["y'"]))
-        , ("bar" @@: ["b"],  "z'" <=: app "bar" ["b"] $
-                             unit @Val ("Int" @: ["z'"]))
-        , ("Int" @: ["x'"], "u1" <=: block (switch "v1"
-                                [ ("foo" @@: ["a1"],  "y1'" <=: app "foo" ["a1"] $
-                                                      unit @Val ("Int" @: ["y1'"]))
-                                , ("bar" @@: ["b1"],  "z1'" <=: app "bar" ["b1"] $
-                                                      unit @Val ("Int" @: ["z1'"]))
-                                , ("Int" @: ["x1'"], unit @Val ("Int" @: ["x1'"]))
-                                ]) $
-                            unit @Val ("Int" @: ["x'"]))
-        ]) $
-      unit @Var "m0"
-
-    after  <- buildExpM $
-      "m0" <=: store @Int 3 $
-      "u" <=: (block $
-                "v'" <=: (block $ switch "v"
-                  [ ("foo" @@: ["a"],  "y'" <=: app "foo" ["a"] $
-                                       unit @Var "y'")
-                  , ("bar" @@: ["b"],  "z'" <=: app "bar" ["b"] $
-                                       unit @Var "z'")
-                  , ("Int" @: ["x'"], "u1" <=: (block $
-                                                "v1'" <=: (block $ switch "v1"
-                                                  [ ("foo" @@: ["a1"],  "y1'" <=: app "foo" ["a1"] $
-                                                                        unit @Var "y1'")
-                                                  , ("bar" @@: ["b1"],  "z1'" <=: app "bar" ["b1"] $
-                                                                        unit @Var "z1'")
-                                                  , ("Int" @: ["x1'"],  unit @Var "x1'")
-                                                  ]) $
-                                                unit @Val ("Int" @: ["v1'"])) $
-                                      unit @Var "x'")
-                  ]) $
-                unit @Val ("Int" @: ["v'"])) $
-      unit @Var "m0"
-
+    let before = [expr|
+            m0 <- store 3
+            u  <- do
+              case v of
+                (Ffoo a)  -> y' <- foo a
+                             pure (CInt y')
+                (Fbar b)  -> z' <- bar b
+                             pure (CInt z')
+                (CInt x') -> u1 <- do
+                               case v1 of
+                                 (Ffoo a1)  -> y1' <- foo a1
+                                               pure (CInt y1')
+                                 (Fbar b1)  -> z1' <- bar b1
+                                               pure (CInt z1')
+                                 (CInt x1') -> pure (CInt x1')
+                             pure (CInt x')
+            pure m0
+          |]
+    let after = [expr|
+            m0 <- store 3
+            u  <- do
+              v' <- do
+                case v of
+                  (Ffoo a)  -> y' <- foo a
+                               pure y'
+                  (Fbar b)  -> z' <- bar b
+                               pure z'
+                  (CInt x') -> u1 <- do
+                                 v1' <- do
+                                   case v1 of
+                                     (Ffoo a1)  -> y1' <- foo a1
+                                                   pure y1'
+                                     (Fbar b1)  -> z1' <- bar b1
+                                                   pure z1'
+                                     (CInt x1') -> pure x1'
+                                 pure (CInt v1')
+                               pure x'
+              pure (CInt v')
+            pure m0
+          |]
     caseCopyPropagation before `sameAs` after
 
   it "Embedded bad case" $ do
-    before <- buildExpM $
-      "m0" <=: store @Int 3 $
-      "u" <=: block (switch "v"
-        [ ("foo" @@: ["a"],  "y'" <=: app "foo" ["a"] $
-                             unit @Val ("Int" @: ["y'"]))
-        , ("bar" @@: ["b"],  "z'" <=: app "bar" ["b"] $
-                             unit @Val ("Int" @: ["z'"]))
-        , ("Int" @: ["x'"], "u1" <=: block (switch "v1"
-                                [ ("foo" @@: ["a1"],  "y1'" <=: app "foo" ["a1"] $
-                                                      unit @Val ("Int" @: ["y1'"]))
-                                , ("bar" @@: ["b1"],  "z1'" <=: app "bar" ["b1"] $
-                                                      unit @Val ("Float" @: ["z1'"]))
-                                , ("Int" @: ["x1'"], unit @Val ("Int" @: ["x1'"]))
-                                ]) $
-                            unit @Val ("Int" @: ["x'"]))
-        ]) $
-      unit @Var "m0"
+    let before = [expr|
+            m0 <- store 3
+            u  <- do
+              case v of
+                (Ffoo a)  -> y' <- foo a
+                             pure (CInt y')
+                (Fbar b)  -> z' <- bar b
+                             pure (CInt z')
+                (CInt x') -> u1 <- do
+                               case v1 of
+                                 (Ffoo a1)  -> y1' <- foo a1
+                                               pure (CInt y1')
+                                 (Fbar b1)  -> z1' <- bar b1
+                                               pure (CFloat z1')
+                                 (CInt x1') -> pure (CInt x1')
+                             pure (CInt x')
+            pure m0
+          |]
 
-    after  <- buildExpM $
-      "m0" <=: store @Int 3 $
-      "u" <=: (block $
-                "v'" <=: (block $ switch "v"
-                  [ ("foo" @@: ["a"],  "y'" <=: app "foo" ["a"] $
-                                       unit @Var "y'")
-                  , ("bar" @@: ["b"],  "z'" <=: app "bar" ["b"] $
-                                       unit @Var "z'")
-                  , ("Int" @: ["x'"], "u1" <=: block (switch "v1"
-                                        [ ("foo" @@: ["a1"],  "y1'" <=: app "foo" ["a1"] $
-                                                              unit @Val ("Int" @: ["y1'"]))
-                                        , ("bar" @@: ["b1"],  "z1'" <=: app "bar" ["b1"] $
-                                                              unit @Val ("Float" @: ["z1'"]))
-                                        , ("Int" @: ["x1'"], unit @Val ("Int" @: ["x1'"]))
-                                        ]) $
-                                      unit @Var "x'")
-                  ]) $
-                unit @Val ("Int" @: ["v'"])) $
-      unit @Var "m0"
-
+    let after = [expr|
+            m0 <- store 3
+            u  <- do
+              v' <- do
+                case v of
+                  (Ffoo a)  -> y' <- foo a
+                               pure y'
+                  (Fbar b)  -> z' <- bar b
+                               pure z'
+                  (CInt x') -> u1 <- do
+                                 case v1 of
+                                   (Ffoo a1)  -> y1' <- foo a1
+                                                 pure (CInt y1')
+                                   (Fbar b1)  -> z1' <- bar b1
+                                                 pure (CFloat z1')
+                                   (CInt x1') -> pure (CInt x1')
+                               pure x'
+              pure (CInt v')
+            pure m0
+          |]
     caseCopyPropagation before `sameAs` after
 
-  it "Leave the outher transform the inner" $ do
-    before <- buildExpM $
-      "m0" <=: store @Int 3 $
-      "u" <=: block (switch "v"
-        [ ("foo" @@: ["a"],  "y'" <=: app "foo" ["a"] $
-                             unit @Val ("Int" @: ["y'"]))
-        , ("bar" @@: ["b"],  "z'" <=: app "bar" ["b"] $
-                             unit @Val ("Float" @: ["z'"]))
-        , ("Int" @: ["x'"], "u1" <=: block (switch "v1"
-                                [ ("foo" @@: ["a1"],  "y1'" <=: app "foo" ["a1"] $
-                                                      unit @Val ("Int" @: ["y1'"]))
-                                , ("bar" @@: ["b1"],  "z1'" <=: app "bar" ["b1"] $
-                                                      unit @Val ("Int" @: ["z1'"]))
-                                , ("Int" @: ["x1'"], unit @Val ("Int" @: ["x1'"]))
-                                ]) $
-                            unit @Val ("Int" @: ["x'"]))
-        ]) $
-      unit @Var "m0"
-
-    after <- buildExpM $
-      "m0" <=: store @Int 3 $
-      "u" <=: block (switch "v"
-        [ ("foo" @@: ["a"],  "y'" <=: app "foo" ["a"] $
-                             unit @Val ("Int" @: ["y'"]))
-        , ("bar" @@: ["b"],  "z'" <=: app "bar" ["b"] $
-                             unit @Val ("Float" @: ["z'"]))
-                  , ("Int" @: ["x'"], "u1" <=: (block $
-                                                "v1'" <=: (block $ switch "v1"
-                                                  [ ("foo" @@: ["a1"],  "y1'" <=: app "foo" ["a1"] $
-                                                                        unit @Var "y1'")
-                                                  , ("bar" @@: ["b1"],  "z1'" <=: app "bar" ["b1"] $
-                                                                        unit @Var "z1'")
-                                                  , ("Int" @: ["x1'"],  unit @Var "x1'")
-                                                  ]) $
-                                                unit @Val ("Int" @: ["v1'"])) $
-                                      unit @Val ("Int" @: ["x'"]))
-        ]) $
-      unit @Var "m0"
-
+  it "Leave the outher, transform the inner" $ do
+    let before = [expr|
+            m0 <- store 3
+            u  <- do
+              case v of
+                (Ffoo a)  -> y' <- foo a
+                             pure (CInt y')
+                (Fbar b)  -> z' <- bar b
+                             pure (CFloat z')
+                (CInt x') -> u1 <- do
+                               case v1 of
+                                 (Ffoo a1)  -> y1' <- foo a1
+                                               pure (CInt y1')
+                                 (Fbar b1)  -> z1' <- bar b1
+                                               pure (CInt z1')
+                                 (CInt x1') -> pure (CInt x1')
+                             pure (CInt x')
+            pure m0
+          |]
+    let after = [expr|
+            m0 <- store 3
+            u  <- do
+              case v of
+                (Ffoo a)  -> y' <- foo a
+                             pure (CInt y')
+                (Fbar b)  -> z' <- bar b
+                             pure (CFloat z')
+                (CInt x') -> u1 <- do
+                               v1' <- do
+                                 case v1 of
+                                   (Ffoo a1)  -> y1' <- foo a1
+                                                 pure y1'
+                                   (Fbar b1)  -> z1' <- bar b1
+                                                 pure z1'
+                                   (CInt x1') -> pure x1'
+                               pure (CInt v1')
+                             pure (CInt x')
+            pure m0
+          |]
     caseCopyPropagation before `sameAs` after
 
   it "last expression is a case" $ do
-
     let before =
           [def|
             sum l =
@@ -206,7 +206,6 @@ spec = do
                                 ax' <- _prim_int_add x' s'
                                 pure (CInt ax')
           |]
-
     let after =
           [def|
             sum l =
@@ -220,7 +219,6 @@ spec = do
                                   pure ax'
               pure (CInt l2')
           |]
-
     caseCopyPropagation before `sameAs` after
 
 runTests :: IO ()
