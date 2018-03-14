49a388c5ac2d2f7a25ea1f291d04aa465840bf6c
Andor Penzes <andor.penzes@gmail.com>
2018-03-15 00:44:00 +0100

    More readable tests for RightHoistFetch

--- grin/test/Transformations/Simplifying/RightHoistFetchSpec.hs
+++ grin/test/Transformations/Simplifying/RightHoistFetchSpec.hs
@@ -1,5 +1,4 @@
-{-# LANGUAGE TypeApplications, OverloadedStrings, LambdaCase #-}
-
+{-# LANGUAGE OverloadedStrings, LambdaCase, QuasiQuotes #-}
 module Transformations.Simplifying.RightHoistFetchSpec where
 
 import Control.Monad
@@ -7,44 +6,44 @@ import Test.Hspec
 import Test hiding (asVal)
 import Test.QuickCheck.Property
 
-import Free
 import Grin
+import GrinTH
 import Assertions
 import Transformations.Simplifying.RightHoistFetch2
 
 spec :: Spec
 spec = do
   it "Example from Figure 4.16" $ do
-    before <- buildExpM $
-      "l0" <=: store @Int 0        $
-      "p"  <=: store (constTagNode "Cons" (asVal @Int <$> [1, 2])) $
-      "t"  <=: fetch "p" (Just 0)  $
-      "a1" <=: fetch "p" (Just 1)  $
-      "a2" <=: fetch "p" (Just 2)  $
-      "l1" <=: store @Int 1        $
-      switch "t"
-        [ (tag "Nil" 0,  "l3" <=: store @Int 2 $
-                         unit @Int 2)
-        , (tag "Cons" 2, "l4" <=: store @Int 3     $
-                         "l5" <=: store @Var "a1"  $
-                         "l6" <=: store @Var "a2"  $
-                         unit @Int 3)
-        ]
-    after <- buildExpM $
-      "l0" <=: store @Int 0        $
-      "p"  <=: store (constTagNode "Cons" (asVal @Int <$> [1, 2])) $
-      "t"  <=: fetch "p" (Just 0)  $
-      "l1" <=: store @Int 1        $
-      switch "t"
-        [ (tag "Nil" 0,  "l3" <=: store @Int 2 $
-                         unit @Int 2)
-        , (tag "Cons" 2, "a1.a1" <=: fetch "p" (Just 1)  $
-                         "a2.a1" <=: fetch "p" (Just 2)  $
-                         "l4" <=: store @Int 3           $
-                         "l5" <=: store @Var "a1.a1"     $
-                         "l6" <=: store @Var "a2.a1"     $
-                         unit @Int 3)
-        ]
+    let before = [expr|
+            l0 <- store 0
+            p  <- store (CCons 1 2)
+            t  <- fetch p[0]
+            a1 <- fetch p[1]
+            a2 <- fetch p[2]
+            l1 <- store 1
+            case t of
+              CNil -> l3 <- store 2
+                      unit 2
+              CCons -> l4 <- store 3
+                       l5 <- store a1
+                       l6 <- store a2
+                       pure 3
+          |]
+    let after = [expr|
+            l0 <- store 0
+            p  <- store (CCons 1 2)
+            t  <- fetch p[0]
+            l1 <- store 1
+            case t of
+              CNil -> l3 <- store 2
+                      pure 2
+              CCons -> a1a1 <- fetch p[1]
+                       a2a1 <- fetch p[2]
+                       l4 <- store 3
+                       l5 <- store a1a1
+                       l6 <- store a2a1
+                       pure 3
+          |]
     rightHoistFetch before `sameAs` after
 
   forM_ programGenerators $ \(name, gen) -> do
