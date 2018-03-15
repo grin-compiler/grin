dd6b0364be13f071426c5739d50f536bedb90f37
Andor Penzes <andor.penzes@gmail.com>
2018-03-15 01:11:13 +0100

    More readable tests for SplitFetch

--- grin/test/Transformations/Simplifying/SplitFetchSpec.hs
+++ grin/test/Transformations/Simplifying/SplitFetchSpec.hs
@@ -1,5 +1,4 @@
-{-# LANGUAGE TypeApplications, OverloadedStrings, LambdaCase #-}
-
+{-# LANGUAGE OverloadedStrings, LambdaCase, QuasiQuotes #-}
 module Transformations.Simplifying.SplitFetchSpec where
 
 import Control.Monad
@@ -7,8 +6,8 @@ import Test.Hspec
 import Test hiding (asVal)
 import Test.QuickCheck.Property
 
-import Free
 import Grin
+import GrinTH
 import Test
 import Assertions
 import Transformations.Simplifying.SplitFetch
@@ -17,54 +16,54 @@ import Transformations.Simplifying.SplitFetch
 spec :: Spec
 spec = do
   it "Example from Figure 4.13" $ do
-    before <- buildExpM $
-      "l1" <=: store @Int 3                                       $
-      (VarTagNode "t" ["a1", "a2", "a3"]) <=: fetch "p" Nothing   $
-      "l2" <=: store @Int 4                                       $
-      unit @Int 5
-
-    after <- buildExpM $
-      "l1" <=: store @Int 3        $
-      "t"  <=: fetch "p" (Just 0)  $
-      "a1" <=: fetch "p" (Just 1)  $
-      "a2" <=: fetch "p" (Just 2)  $
-      "a3" <=: fetch "p" (Just 3)  $
-      "l2" <=: store @Int 4        $
-      unit @Int 5
-
+    let before = [expr|
+            l1 <- store 3
+            (t a1 a2 a3) <- fetch p
+            l2 <- store 4
+            pure 5
+          |]
+    let after = [expr|
+            l1 <- store 3
+            t  <- fetch p[0]
+            a1 <- fetch p[1]
+            a2 <- fetch p[2]
+            a3 <- fetch p[3]
+            l2 <- store 4
+            pure 5
+          |]
     splitFetch before `sameAs` after
 
   it "Example from Figure 4.14" $ do
-    before <- buildExpM $
-      "l1" <=: store @Int 3                                       $
-      ("Int" #: ["x"]) <=: fetch "p" Nothing    $
-      "l2" <=: store @Int 4                                       $
-      unit @Int 5
-
-    after <- buildExpM $
-      "l1" <=: store @Int 3         $
-      "x"  <=: fetch "p" (Just 1)   $
-      "l2" <=: store @Int 4         $
-      unit @Int 5
-
+    let before = [expr|
+            l1 <- store 3
+            (CInt x) <- fetch p
+            l2 <- store 4
+            pure 5
+          |]
+    let after = [expr|
+            l1 <- store 3
+            x  <- fetch p[1]
+            l2 <- store 4
+            pure 5
+          |]
     splitFetch before `sameAs` after
 
   it "Example from Figure 4.15" $ do
     -- TODO Include hpt-result with t \elem { CPair }
-    before <- buildExpM $
-      "l1" <=: store @Int 3                                 $
-      (VarTagNode "t" ["a1", "a2"]) <=: fetch "p" Nothing   $
-      "l2" <=: store @Int 4                                 $
-      unit @Int 5
-
-    after <- buildExpM $
-      "l1" <=: store @Int 3             $
-      "t"  <=: unit @Val (tag' "Pair")  $
-      "a1" <=: fetch "p" (Just 1)       $
-      "a2" <=: fetch "p" (Just 2)       $
-      "l2" <=: store @Int 4             $
-      unit @Int 5
-
+    let before = [expr|
+            l1 <- store 3
+            (t a1 a2) <- fetch p
+            l2 <- store 4
+            pure 5
+          |]
+    let after = [expr|
+            l1 <- store 3
+            t  <- pure CPair
+            a1 <- fetch p[1]
+            a2 <- fetch p[2]
+            l2 <- store 4
+            pure 5
+          |]
     splitFetch before `sameAs` after
 
   forM_ programGenerators $ \(name, gen) -> do
