dd33ab0c56dbf76b73e0931d97483438043b0747
Csaba Hruska <csaba.hruska@gmail.com>
2018-04-18 00:50:01 +0200

    more work on lambda codegen

--- grin/src/Frontend/CodeGen.hs
+++ grin/src/Frontend/CodeGen.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE LambdaCase, TupleSections #-}
 module Frontend.CodeGen (codegenGrin) where
 
+import Text.Printf
 import Control.Monad
 import Control.Monad.State
 
@@ -11,13 +12,13 @@ import qualified Data.Map as Map
 import Frontend.Lambda
 import qualified Grin as G
 
-type CG = State ()
+type CG = State (Map Name Int) -- TODO
 
 uniq :: Name -> CG Name
-uniq name = undefined
+uniq name = pure name -- TODO
 
-arity :: Name -> Maybe Int
-arity = undefined
+arity :: Map Name Int -> Name -> Maybe Int
+arity = flip Map.lookup -- TODO
 
 genLit :: Lit -> G.Lit
 genLit = \case
@@ -36,16 +37,7 @@ genAtom :: Atom -> G.Val
 genAtom = \case
   Var name  -> G.Var name
   Lit lit   -> G.Lit $ genLit lit
-  x -> error $ show x
-
--- lazy context ; constructs suspended computations
-genC :: Exp -> CG G.Exp
-genC = \case
-  App name args | argCount <- length args
-                , Just ar <- arity name -> case argCount `compare` ar of
-    EQ  -> pure $ G.SStore $ G.ConstTagNode (G.Tag G.F name) $ map genAtom args
-    LT  -> pure $ G.SStore $ G.ConstTagNode (G.Tag G.P $ name ++ show (ar - argCount)) $ map genAtom args
-  x -> error $ show x
+  x -> error $ printf "unsupported atom: %s" $ show x
 
 apChain :: G.Exp -> [Atom] -> CG G.Exp
 apChain exp args = foldr ap (pure exp) args where -- TODO: fix the order
@@ -53,26 +45,35 @@ apChain exp args = foldr ap (pure exp) args where -- TODO: fix the order
     argWhnf <- uniq "arg"
     G.EBind <$> leftExp <*> pure (G.Var argWhnf) <*> pure (G.SApp "apply" [G.Var argWhnf, genAtom arg])
 
+-- lazy context ; constructs suspended computations
+genC :: Exp -> CG G.Exp
+genC e = get >>= \arityMap -> case e of
+  App name args | argCount <- length args
+                , Just ar <- arity arityMap name -> case argCount `compare` ar of
+    EQ  -> pure $ G.SStore $ G.ConstTagNode (G.Tag G.F name) $ map genAtom args
+    LT  -> pure $ G.SStore $ G.ConstTagNode (G.Tag G.P $ name ++ show (ar - argCount)) $ map genAtom args
+  x -> error $ printf "unsupported C: %s" $ show x
+
 -- strict context ; evaluates to WHNF
 genE :: Exp -> CG G.Exp
-genE = \case
+genE e = get >>= \arityMap -> case e of
   App name args | argCount <- length args
-                , Just ar <- arity name -> case argCount `compare` ar of
+                , Just ar <- arity arityMap name -> case argCount `compare` ar of
     EQ  -> pure $ G.SApp name $ map genAtom args
     LT  -> pure $ G.SReturn $ G.ConstTagNode (G.Tag G.P $ name ++ show (ar - argCount)) $ map genAtom args
     GT  -> let (funArgs, extraArgs) = splitAt ar args
            in apChain (G.SApp name $ map genAtom funArgs) extraArgs
   -- HINT: unknown function ; generate apply chain
   App name args | argCount <- length args
-                , Nothing <- arity name
+                , Nothing <- arity arityMap name
         -- apply chain
         -> apChain (G.SApp "eval" [G.Var name]) args
-  Var name -> pure $ G.SApp "eval" [G.Var name]
-  x -> error $ show x
+  Var name -> pure $ G.SApp "eval" [G.Var name] -- TODO: handle functions
+  x -> error $ printf "unsupported E: %s" $ show x
 
 -- strict and return context (evaluates to WHNF) ; R is similar to E
 genR :: Exp -> CG G.Exp
-genR = \case
+genR e = get >>= \arityMap -> case e of
   -- TODO: build var name <--> location name map for suspended computations
   Let  binds exp -> foldr (\(name, e) rightExp -> G.EBind <$> genC e <*> pure (G.Var name {-TODO-}) <*> rightExp) (genR exp) binds
   LetS binds exp -> foldr (\(name, e) rightExp -> G.EBind <$> genE e <*> pure (G.Var name) <*> rightExp) (genR exp) binds
@@ -82,16 +83,36 @@ genR = \case
   Case exp alts -> G.EBind <$> genE exp <*> pure (genAtom exp) <*> (G.ECase (genAtom exp) <$> mapM genR alts)
   Alt pat exp -> G.Alt (genCPat pat) <$> genR exp
 
-  x -> error $ show x
+  Program defs      -> G.Program <$> mapM genR defs
+  Def name args exp -> G.Def name args <$> genR exp
+
+  Lit lit -> pure $ G.SReturn $ G.Lit $ genLit lit
+  App name args | argCount <- length args
+                , Just ar <- arity arityMap name -> case argCount `compare` ar of
+    EQ  -> pure $ G.SApp name $ map genAtom args
+    LT  -> pure $ G.SReturn $ G.ConstTagNode (G.Tag G.P $ name ++ show (ar - argCount)) $ map genAtom args
+    GT  -> let (funArgs, extraArgs) = splitAt ar args
+           in apChain (G.SApp name $ map genAtom funArgs) extraArgs
+  -- HINT: unknown function ; generate apply chain
+  App name args | argCount <- length args
+                , Nothing <- arity arityMap name
+        -- apply chain
+        -> apChain (G.SApp "eval" [G.Var name]) args
+
+  x -> error $ printf "unsupported R: %s" $ show x
 
 {-
   TODO:
     done - unknown function: apply chain
     done - over application
-    - rewrite as Grin anamorphism
     - generate eval function
     - generate apply function
     - letrec and circular data structures
+    - primop
+    - fill R, E, C
+    - rewrite as Grin anamorphism
+    - higher order sample
+    - circular data sample
 -}
 
 {-
@@ -105,8 +126,8 @@ genR = \case
   Alt   R
 -}
 
-codegenGrin :: Exp -> G.Exp
-codegenGrin _ = G.Program [] -- TODO
+codegenGrin :: Program -> G.Program
+codegenGrin exp = evalState (genR exp) (buildArityMap exp)
 
 {-
   = Program     [Def]
