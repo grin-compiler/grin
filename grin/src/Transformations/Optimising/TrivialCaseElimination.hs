f1278a1c5ec46d7791fa7dedfbb4866414120ff8
Csaba Hruska <csaba.hruska@gmail.com>
2018-04-25 12:24:42 +0200

    cleanup

--- grin/src/Transformations/Optimising/TrivialCaseElimination.hs
+++ grin/src/Transformations/Optimising/TrivialCaseElimination.hs
@@ -9,6 +9,6 @@ trivialCaseElimination :: Exp -> Exp
 trivialCaseElimination = ana builder where
   builder :: Exp -> ExpF Exp
   builder = \case
-    ECase val [Alt DefaultPat body] -> SBlockF $ EBind (SReturn Unit) Unit              body
+    ECase val [Alt DefaultPat body] -> SBlockF body
     ECase val [Alt cpat body]       -> SBlockF $ EBind (SReturn val)  (cpatToLPat cpat) body
     exp -> project exp
