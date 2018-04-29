37d00e846016c103d3cf2ba1ce6eb4f78900b3ed
Csaba Hruska <csaba.hruska@gmail.com>
2018-04-29 21:57:38 +0200

    save llvm before and after at opt stages

--- grin/grin/opt-stages-high-level/pp-min.sh
+++ grin/grin/opt-stages-high-level/pp-min.sh
@@ -1,9 +1,10 @@
 stack exec -- grin -o . -p \
   --bn --cnp --hpt \
+  --save-llvm simple \
   --cse --cfl --dve \
   --ar --cfl --dve \
   --hpt \
   --ar --cfl --dve \
   --hpt -p --llvm \
-  --save-llvm opt \
+  --save-llvm opt --ast \
   stage-06.grin
