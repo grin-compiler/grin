9d5380bf18fabe2cc031a3c3edd6216148ebe239
Csaba Hruska <csaba.hruska@gmail.com>
2018-05-06 17:02:02 +0200

    unit tests for dead procedure elimination

---
+++ grin/test/Transformations/Optimising/DeadProcedureEliminationSpec.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE OverloadedStrings, QuasiQuotes, ViewPatterns #-}
+module Transformations.Optimising.DeadProcedureEliminationSpec where
+
+import Transformations.Optimising.DeadProcedureElimination
+
+import Test.Hspec
+import GrinTH
+import Test hiding (newVar)
+import Assertions
+
+
+runTests :: IO ()
+runTests = hspec spec
+
+spec :: Spec
+spec = do
+  it "simple" $ do
+    let before = [prog|
+        grinMain =
+          funA 1
+          funB 2
+
+        funA a = pure ()
+        funB b = funC b
+        funC c = pure ()
+
+        deadFunA d = pure d
+        deadFunB e = deadFunA e
+      |]
+    let after = [prog|
+        grinMain =
+          funA 1
+          funB 2
+
+        funA a = pure ()
+        funB b = funC b
+        funC c = pure ()
+      |]
+    deadProcedureElimination before `sameAs` after
+
+  it "reference direction" $ do
+    let before = [prog|
+        grinMain =
+          funA 1
+
+        funA b = funB b
+        funB c = pure ()
+
+        deadFunA d = funA d
+        deadFunB e = deadFunA e
+      |]
+    let after = [prog|
+        grinMain =
+          funA 1
+
+        funA b = funB b
+        funB c = pure ()
+      |]
+    deadProcedureElimination before `sameAs` after
