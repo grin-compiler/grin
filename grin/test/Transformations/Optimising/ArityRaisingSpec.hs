d51091de13144dce4559da03d58c26b024d70f3c
Csaba Hruska <csaba.hruska@gmail.com>
2018-05-09 22:49:24 +0200

    move arity raising bug test to unit tests

--- grin/test/Transformations/Optimising/ArityRaisingSpec.hs
+++ grin/test/Transformations/Optimising/ArityRaisingSpec.hs
@@ -307,3 +307,53 @@ spec = do
           _prim_int_print m'
       |]
     snd (arityRaising (inferTypeEnv before, before)) `sameAs` after
+
+  it "bugfix - multi indirection" $ do
+    let before = [prog|
+        grinMain =
+          p2 <- store (CInt 1)
+          p3 <- store (CInt 1000)
+          p4 <- store (Fupto p2 p3)
+          n13' <- sum 0 p4
+          _prim_int_print n13'
+
+        sum p101 p11 =
+          (Fupto p17 p18) <- fetch p11
+          (CInt n2') <- fetch p17
+          (CInt n3') <- fetch p18
+          b1' <- _prim_int_gt n2' n3'
+          case b1' of
+            #True ->
+              pure p101
+            #False ->
+              n4' <- _prim_int_add n2' 1
+              p8 <- store (CInt n4')
+              p9 <- store (Fupto p8 p18)
+              n7'_2 <- _prim_int_add p101 n2'
+              sum n7'_2 p9
+      |]
+    let after = [prog|
+        grinMain =
+          p2 <- store (CInt 1)
+          p3 <- store (CInt 1000)
+          p4 <- store (Fupto p2 p3)
+          n13' <- sum 0 p2 p3
+          _prim_int_print n13'
+
+        sum p101 p111 p112 =
+          (Fupto p17 p18) <- pure (Fupto p111 p112)
+          (CInt n2') <- fetch p17
+          (CInt n3') <- fetch p18
+          b1' <- _prim_int_gt n2' n3'
+          case b1' of
+            #True ->
+              pure p101
+            #False ->
+              n4' <- _prim_int_add n2' 1
+              p8 <- store (CInt n4')
+              p9 <- store (Fupto p8 p18)
+              n7'_2 <- _prim_int_add p101 n2'
+              sum n7'_2 p8 p18
+      |]
+    snd (arityRaising (inferTypeEnv before, before)) `sameAs` after
+
