468920e6dac7d5e2532f84ba4f43c2b6994aabf3
Csaba Hruska <csaba.hruska@gmail.com>
2018-05-10 19:58:49 +0200

    fixed constant propagation ; add unit tests

--- grin/src/Transformations/Optimising/ConstantPropagation.hs
+++ grin/src/Transformations/Optimising/ConstantPropagation.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE LambdaCase, TupleSections #-}
+{-# LANGUAGE LambdaCase, TupleSections, ViewPatterns #-}
 module Transformations.Optimising.ConstantPropagation where
 
 import Text.Printf
@@ -9,28 +9,43 @@ import Grin
 import Pretty
 import Transformations.Util
 
-type Env = (Map Val Val)
+type Env = Map Val Val
 
--- TODO: handle returning case
 constantPropagation :: Exp -> Exp
 constantPropagation e = ana builder (mempty, e) where
 
   builder :: (Env, Exp) -> ExpF (Env, Exp)
-  builder (env, exp) = case substVals env exp of
-    ECase val alts | isKnown val -> (env,) <$> EBindF (SReturn val) (cpatToLPat cpat) body where
-      -- NOTE: there must be only one matching alternative!
-      Alt cpat body = case [alt | alt@(Alt cpat body) <- alts, match cpat val] of
-        [alt] -> alt
-        _ -> error $ printf "illegal case expression\n%s" (show $ pretty exp)
+  builder (env, exp) = case exp of
+    ECase val alts ->
+      let constVal      = subst env val
+          known         = isKnown constVal || Map.member val env
+          matchingAlts  = [alt | alt@(Alt cpat body) <- alts, match cpat constVal]
+          defaultAlts   = [alt | alt@(Alt DefaultPat body) <- alts]
+          -- HINT: use cpat as known value in the alternative ; bind cpat to val
+          altEnv cpat   = env `mappend` unify env (cpatToLPat cpat) val
+      in case (known, matchingAlts, defaultAlts) of
+        -- known scutinee, specific pattern
+        (True, [Alt cpat body], _)        -> (env,) <$> EBindF (SReturn constVal) (cpatToLPat cpat) body
 
-    -- HINT: in each alternative set val value like it was matched
-    -- TODO: do proper unification
-    ECase val alts -> ECaseF val [(Map.insert val (cpatToVal cpat) env, alt) | alt@(Alt cpat _) <- alts]
+        -- known scutinee, default pattern
+        (True, _, [Alt DefaultPat body])  -> (env,) <$> EBindF (SReturn Unit) Unit body
+
+        -- unknown scutinee
+        -- HINT: in each alternative set val value like it was matched
+        _ -> ECaseF val [(altEnv cpat, alt) | alt@(Alt cpat _) <- alts]
 
     -- track values
-    EBind (SReturn val) lpat rightExp -> (Map.insert lpat (subst env val) env,) <$> project exp
+    EBind (SReturn val) lpat _rightExp -> (newEnv,) <$> project exp where
+      newEnv = env `mappend` unify env val lpat
 
     _ -> (env,) <$> project exp
+
+  unify :: Env -> Val -> LPat -> Env
+  unify env (subst env -> val) lpat = case (lpat, val) of
+    (ConstTagNode lpatTag lpatArgs, ConstTagNode valTag valArgs)
+      | lpatTag == valTag     -> mconcat $ zipWith (unify env) valArgs lpatArgs
+    (Var{}, _)                -> Map.singleton lpat val
+    _                         -> mempty -- LPat: unit, lit, tag
 
   isKnown :: Val -> Bool
   isKnown = \case
@@ -38,9 +53,6 @@ constantPropagation e = ana builder (mempty, e) where
     Lit{}          -> True
     ValTag{}       -> True
     _              -> False
-
-  cpatToVal :: CPat -> Val
-  cpatToVal = cpatToLPat
 
   match :: CPat -> Val -> Bool
   match (NodePat tagA _) (ConstTagNode tagB _) = tagA == tagB
---
+++ grin/test/Transformations/Optimising/ConstantPropagationSpec.hs
@@ -0,0 +1,168 @@
+{-# LANGUAGE OverloadedStrings, QuasiQuotes, ViewPatterns #-}
+module Transformations.Optimising.ConstantPropagationSpec where
+
+import Transformations.Optimising.ConstantPropagation
+
+import Test.Hspec
+import GrinTH
+import Test hiding (newVar)
+import Assertions
+
+
+runTests :: IO ()
+runTests = hspec spec
+
+
+spec :: Spec
+spec = do
+  it "base case" $ do
+    let before = [expr|
+        i1 <- pure 1
+        n1 <- pure (CNode i1)
+        case n1 of
+          (CNil)      -> pure 1
+          (CNode a1)  -> pure 2
+      |]
+    let after = [expr|
+        i1 <- pure 1
+        n1 <- pure (CNode i1)
+        (CNode a1) <- pure (CNode i1)
+        pure 2
+      |]
+    constantPropagation before `sameAs` after
+
+  it "ignore illformed case - multi matching" $ do
+    let before = [expr|
+        i1 <- pure 1
+        n1 <- pure (CNode i1)
+        case n1 of
+          (CNil)      -> pure 1
+          (CNode a1)  -> pure 2
+          (CNode b1)  -> pure 3
+        case n1 of
+          (CNil)    -> pure 4
+          #default  -> pure 5
+          #default  -> pure 6
+      |]
+    let after = [expr|
+        i1 <- pure 1
+        n1 <- pure (CNode i1)
+        case n1 of
+          (CNil)      -> pure 1
+          (CNode a1)  -> pure 2
+          (CNode b1)  -> pure 3
+        case n1 of
+          (CNil)    -> pure 4
+          #default  -> pure 5
+          #default  -> pure 6
+      |]
+    constantPropagation before `sameAs` after
+
+  it "default pattern" $ do
+    let before = [expr|
+        i1 <- pure 1
+        n1 <- pure (CNode i1)
+        case n1 of
+          (CNil)    -> pure 2
+          #default  -> pure 3
+      |]
+    let after = [expr|
+        i1 <- pure 1
+        n1 <- pure (CNode i1)
+        pure ()
+        pure 3
+      |]
+    constantPropagation before `sameAs` after
+
+  it "unknown scrutinee - simple" $ do
+    let before = [expr|
+        case n1 of
+          (CNil)    -> pure 2
+          #default  -> pure 3
+      |]
+    let after = [expr|
+        case n1 of
+          (CNil)    -> pure 2
+          #default  -> pure 3
+      |]
+    constantPropagation before `sameAs` after
+
+  it "unknown scrutinee becomes known in alternatives - specific pattern" $ do
+    let before = [expr|
+        case n1 of
+          (CNil) ->
+            case n1 of
+              (CNil)      -> pure 1
+              (CNode a1)  -> pure 2
+          (CNode a2) ->
+            case n1 of
+              (CNil)      -> pure 3
+              (CNode a3)  -> pure 4
+      |]
+    let after = [expr|
+        case n1 of
+          (CNil) ->
+            (CNil) <- pure (CNil)
+            pure 1
+          (CNode a2) ->
+            (CNode a3) <- pure (CNode a2)
+            pure 4
+      |]
+    constantPropagation before `sameAs` after
+
+  it "unknown scrutinee becomes known in alternatives - default pattern" $ do
+    let before = [expr|
+        case n1 of
+          #default ->
+            case n1 of
+              #default    -> pure 1
+              (CNode a1)  -> pure 2
+          (CNode a2) ->
+            case n1 of
+              #default    -> pure 3
+              (CNode a3)  -> pure 4
+      |]
+    let after = [expr|
+        case n1 of
+          #default ->
+            pure ()
+            pure 1
+          (CNode a2) ->
+            (CNode a3) <- pure (CNode a2)
+            pure 4
+      |]
+    constantPropagation before `sameAs` after
+
+  it "literal - specific pattern" $ do
+    let before = [expr|
+        i1 <- pure 1
+        case i1 of
+          (CNil)      -> pure 1
+          (CNode a1)  -> pure 2
+          1           -> pure 3
+          2           -> pure 4
+          #default    -> pure 5
+      |]
+    let after = [expr|
+        i1 <- pure 1
+        1 <- pure 1
+        pure 3
+      |]
+    constantPropagation before `sameAs` after
+
+  it "literal - default pattern" $ do
+    let before = [expr|
+        i1 <- pure 3
+        case i1 of
+          (CNil)      -> pure 1
+          (CNode a1)  -> pure 2
+          1           -> pure 3
+          2           -> pure 4
+          #default    -> pure 5
+      |]
+    let after = [expr|
+        i1 <- pure 3
+        pure ()
+        pure 5
+      |]
+    constantPropagation before `sameAs` after
