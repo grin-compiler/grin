3a555213da2f8359552dd93ccc13d85370862780
Csaba Hruska <csaba.hruska@gmail.com>
2018-05-31 22:28:37 +0200

    disable lambda builtins

--- grin/src/Frontend/Lambda/PrimOps.hs
+++ grin/src/Frontend/Lambda/PrimOps.hs
@@ -4,26 +4,29 @@ module Frontend.Lambda.PrimOps where
 import GrinTH
 
 lambdaPrimOps = [prog|
+  {-
   ap ap_f ap_x =
     ap_v <- eval ap_f
     apply ap_v ap_x
-
-  int_gt int_gt0 int_gt1 =
-    (CInt int_gt0_1) <- eval int_gt0
-    (CInt int_gt1_1) <- eval int_gt1
-    int_gt2 <- _prim_int_gt int_gt0_1 int_gt1_1
-    case int_gt2 of
+  -}
+  {-
+  _rts_int_gt p0$ p1$ =
+    (CInt int0$) <- eval p0$
+    (CInt int1$) <- eval p1$
+    bool0$ <- _prim_int_gt int0$ int1$
+    case bool0$ of
       #False  -> pure (CFalse)
       #True   -> pure (CTrue)
 
-  int_print int_print0 =
-    (CInt int_print0_1) <- eval int_print0
-    _prim_int_print int_print0_1
+  _rts_int_print p2$ =
+    (CInt int2$) <- eval p2$
+    _prim_int_print int2$
     pure (CUnit)
 
-  int_add int_add0 int_add1 =
-    (CInt int_add0_1) <- eval int_add0
-    (CInt int_add1_1) <- eval int_add1
-    int_add2 <- _prim_int_add int_add0_1 int_add1_1
-    pure (CInt int_add2)
+  _rts_int_add p3$ p4$ =
+    (CInt int3$) <- eval p3$
+    (CInt int4$) <- eval p4$
+    int5$ <- _prim_int_add int3$ int4$
+    pure (CInt int5$)
+  -}
 |]
