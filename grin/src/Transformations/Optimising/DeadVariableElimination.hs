f13a9af831d7e43ceef45a93b7c160aa1aa774bd
Csaba Hruska <csaba.hruska@gmail.com>
2018-05-31 21:37:38 +0200

    fix dead variable elimination: use type env to keep effectful value bindings

--- grin/src/Transformations/Optimising/DeadVariableElimination.hs
+++ grin/src/Transformations/Optimising/DeadVariableElimination.hs
@@ -7,15 +7,17 @@ import qualified Data.Set as Set
 import Data.Functor.Foldable as Foldable
 import qualified Data.Foldable
 import Grin
+import TypeEnv
 import Transformations.Util
 
-deadVariableElimination :: Exp -> Exp
-deadVariableElimination = fst . cata folder where
+deadVariableElimination :: (TypeEnv, Exp) -> (TypeEnv, Exp)
+deadVariableElimination (typeEnv, e) = (typeEnv, fst $ cata folder e) where
   folder :: ExpF (Exp, Set Name) -> (Exp, Set Name)
   folder = \case
     EBindF _ lpat right@(_, rightRef)
       | lpat /= Unit
       , vars <- foldNamesVal Set.singleton lpat
+      , all ((/=) unit_t . variableType typeEnv) vars
       , all (flip Set.notMember rightRef) vars
       -> right
     exp -> (embed $ fmap fst exp, foldNameUseExpF Set.singleton exp `mappend` Data.Foldable.fold (fmap snd exp))
