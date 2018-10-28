72cd57a6ccc0eb825099b1bf364138540dfced09
Csaba Hruska <csaba.hruska@gmail.com>
2018-10-28 20:27:52 +0100

    simplify effect map calculation algebra ; add notes

--- grin/src/Grin/TypeEnv.hs
+++ grin/src/Grin/TypeEnv.hs
@@ -164,8 +164,8 @@ typeOfValTE typeEnv = \case
 -- * Effects
 
 data Effect
-  = Effectful Name
+  = Effectful Name                      -- called effectful primop or function name
   | Update { updateLocs :: [Int] }
   deriving (Eq, Show, Ord)
 
-type EffectMap = Map Name (Set Effect)
+type EffectMap = Map Name (Set Effect)  -- key: function name, value: effectful calls or updates
