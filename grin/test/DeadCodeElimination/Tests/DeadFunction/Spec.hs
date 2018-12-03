664667882d0e564eccea4b1e716cf5af21051e21
Andor Penzes <andor.penzes@gmail.com>
2018-12-03 23:28:06 +0100

    DeadCodeElimination tests: Reduce indirection.

--- grin/test/DeadCodeElimination/Tests/DeadFunction/Spec.hs
+++ grin/test/DeadCodeElimination/Tests/DeadFunction/Spec.hs
@@ -6,6 +6,7 @@ import Data.Either (fromRight)
 
 import Test.IO
 import Test.Hspec
+import Test.Util
 
 import Grin.Grin
 import Grin.TypeCheck
@@ -15,13 +16,25 @@ import Transformations.Optimising.DeadFunctionElimination
 import Transformations.EffectMap
 
 import LiveVariable.LiveVariableSpec (calcLiveness)
+import DeadCodeElimination.Tests.Util
 
-import DeadCodeElimination.Tests.DeadFunction.AppSideEffect1
-import DeadCodeElimination.Tests.DeadFunction.MutuallyRecursive
-import DeadCodeElimination.Tests.DeadFunction.ReplaceNode
-import DeadCodeElimination.Tests.DeadFunction.ReplaceSimpleType
-import DeadCodeElimination.Tests.DeadFunction.Simple
-import DeadCodeElimination.Tests.DeadFunction.TrueSideEffectMin
+
+dfeBefore :: FilePath
+dfeBefore = dceExamples </> "dead_fun" </> "before"
+
+dfeAfter :: FilePath
+dfeAfter = dceExamples </> "dead_fun" </> "after"
+
+-- name ~ name of the test case, and also the grin source file
+mkDFETestCase :: String -> (FilePath, FilePath, FilePath -> Exp -> Spec)
+mkDFETestCase name = mkBeforeAfterTestCase name dfeBefore dfeAfter
+
+(appSideEffect1Before, appSideEffect1After, appSideEffect1Spec) = mkDFETestCase "app_side_effect_1"
+(simpleBefore, simpleAfter, simpleSpec) = mkDFETestCase "simple"
+(replaceSimpleTypeBefore, replaceSimpleTypeAfter, replaceSimpleTypeSpec) = mkDFETestCase "replace_simple_type"
+(replaceNodeBefore, replaceNodeAfter, replaceNodeSpec) = mkDFETestCase "replace_node"
+(mutuallyRecursiveBefore, mutuallyRecursiveAfter, mutuallyRecursiveSpec) = mkDFETestCase "mutually_recursive"
+(trueSideEffectMinBefore, trueSideEffectMinAfter, trueSideEffectMinSpec) = mkDFETestCase "true_side_effect_min"
 
 
 spec :: Spec
@@ -72,5 +85,3 @@ eliminateDeadFunctions e =
     lvaResult = calcLiveness e
     tyEnv = inferTypeEnv e
     effMap = effectMap (tyEnv, e)
-
-
