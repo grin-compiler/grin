aecc87f2c3f580bf2ead3659f992b4da94e62500
Andor Penzes <andor.penzes@gmail.com>
2019-01-06 19:30:07 +0100

    Test: Appropiate callback in PipelineExample.

--- grin/test/Test/Hspec/PipelineExample.hs
+++ grin/test/Test/Hspec/PipelineExample.hs
@@ -16,6 +16,7 @@ import Control.Monad.Trans.Except
 import Control.Monad (when)
 import Control.Monad.Trans
 import Data.List (isSuffixOf)
+import Data.IORef
 import Data.Text.IO as Text
 import System.FilePath ((</>))
 import Test.Hspec.Core.Spec hiding (pending)
@@ -46,32 +47,36 @@ pending = Pending Nothing . Just
 instance Example Pipeline where
   type Arg Pipeline = ()
   evaluateExample (Pipeline{..}) params actionWith progressCallback = do
-    pwd <- getCurrentDirectory
-    -- There is a difference between the 'stack ghci --test' and 'stack test'.
-    -- Stack test uses the grin/grin meanwhile stack ghci uses 'grin' directory
-    let testDataDir = if | "/grin/grin" `isSuffixOf` pwd -> "test-data/"
-                         | "/grin"      `isSuffixOf` pwd -> "grin/test-data/"
-                         | otherwise -> error "Impossible: stack did not run inside the project dir."
-        getInput = \case
-          Src prg -> pure prg
-          File fn -> parseProg <$> Text.readFile (testDataDir </> fn)
+    result <- newIORef $ Result "" Success
+    actionWith $ \() -> do
+      pwd <- getCurrentDirectory
+      -- There is a difference between the 'stack ghci --test' and 'stack test'.
+      -- Stack test uses the grin/grin meanwhile stack ghci uses 'grin' directory
+      let testDataDir = if | "/grin/grin" `isSuffixOf` pwd -> "test-data/"
+                           | "/grin"      `isSuffixOf` pwd -> "grin/test-data/"
+                           | otherwise -> error "Impossible: stack did not run inside the project dir."
+          getInput = \case
+            Src prg -> pure prg
+            File fn -> parseProg <$> Text.readFile (testDataDir </> fn)
 
-    fmap (Result "" . either id (const Success)) $ runExceptT $ do
-      result <- lift $ try $ do
-        (,) <$> getInput before
-            <*> getInput after
-      (beforeExp, afterExp) <- either (throwE . pending . show @SomeException) pure $ result
-      let opts = PipelineOpts
-            { _poOutputDir = ".output" -- TODO: Random test dir
-            , _poFailOnLint = False
-            , _poLogging = False
-            , _poSaveTypeEnv = False
-            , _poStatistics = False
-            , _poLintOnChange = False
-            , _poTypedLint = False
-            }
-      resultExp <- lift $ Pipeline.Pipeline.pipeline opts Nothing beforeExp steps
-      when (afterExp /= resultExp) $ do
-        throwE $ Failure Nothing $ ExpectedButGot Nothing
-          (show $ plain $ pretty afterExp)
-          (show $ plain $ pretty resultExp)
+      res <- fmap (Result "" . either id (const Success)) $ runExceptT $ do
+        result <- lift $ try $ do
+          (,) <$> getInput before
+              <*> getInput after
+        (beforeExp, afterExp) <- either (throwE . pending . show @SomeException) pure $ result
+        let opts = PipelineOpts
+              { _poOutputDir = ".output" -- TODO: Random test dir
+              , _poFailOnLint = False
+              , _poLogging = False
+              , _poSaveTypeEnv = False
+              , _poStatistics = False
+              , _poLintOnChange = False
+              , _poTypedLint = False
+              }
+        resultExp <- lift $ Pipeline.Pipeline.pipeline opts Nothing beforeExp steps
+        when (afterExp /= resultExp) $ do
+          throwE $ Failure Nothing $ ExpectedButGot Nothing
+            (show $ plain $ pretty afterExp)
+            (show $ plain $ pretty resultExp)
+      writeIORef result res
+    readIORef result
