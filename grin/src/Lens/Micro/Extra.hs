ba9d579e260f4ef2c6c5ce37a5b786e28d5780b9
Andor Penzes <andor.penzes@gmail.com>
2019-02-07 23:11:13 +0100

    Add missing combinator from Lens.

--- grin/src/Lens/Micro/Extra.hs
+++ grin/src/Lens/Micro/Extra.hs
@@ -1,9 +1,11 @@
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeFamilies, RankNTypes #-}
 module Lens.Micro.Extra where
 
 import Lens.Micro.Internal
 import Lens.Micro.Platform
 import Data.Vector as V
+import Data.Functor.Infix ((<$$>))
+import Data.Monoid (Any(..))
 
 
 type instance Index   (Vector a) = Int
@@ -11,3 +13,6 @@ type instance IxValue (Vector a) = a
 
 instance At (Vector a) where
   at k = lens (V.!? k) (\v -> maybe v (\a -> v V.// [(k, a)]))
+
+isn't :: Getting Any s a -> s -> Bool
+isn't = not <$$> has
