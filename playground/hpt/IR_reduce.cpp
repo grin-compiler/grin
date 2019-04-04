dba3f65f12a23d8c232d90eb531569fee412959e
Csaba Hruska <csaba.hruska@gmail.com>
2019-04-04 17:12:33 +0200

    implement conditonal move

--- playground/hpt/IR_reduce.cpp
+++ playground/hpt/IR_reduce.cpp
@@ -5,7 +5,7 @@
 
 #include "IR.h"
 
-typedef std::unordered_map<uint32_t, std::vector<std::unordered_set<int32_t>>> node_set_t;
+typedef std::unordered_map<int32_t, std::vector<std::unordered_set<int32_t>>> node_set_t;
 
 // NOTE: GRIN is a typed language, a register can have only one type from the following options: simple_type, location, node
 struct value_t {
@@ -25,8 +25,17 @@ struct computer_state_t {
     reg.resize(p.register_count);
   }
 
+  // predicate functions
+  bool eval_predicate_int(int32_t i, predicate_t& p);
+  bool eval_predicate_tag(int32_t i, predicate_t& p);
+
   // modification functions
   void insert_int_set(std::unordered_set<int32_t>& dst, int32_t value);
+
+  void conditional_union_int_set(std::unordered_set<int32_t>& src, std::unordered_set<int32_t>& dst, predicate_t& p);
+  void conditional_union_node_set(node_set_t& src, node_set_t& dst, predicate_t& p);
+  void conditional_union_value(value_t& src, value_t& dst, predicate_t& p);
+
   void union_int_set(std::unordered_set<int32_t>& src, std::unordered_set<int32_t>& dst);
   void union_node_set(node_set_t& src, node_set_t& dst, bool merge_new_tags = true);
   void union_value(value_t& src, value_t& dst);
@@ -36,6 +45,7 @@ struct computer_state_t {
   void eval_block(block_id_t bid);
 
   // eval commands
+  void eval_conditional_move(cmd_t &c);
   void eval_restricted_move(cmd_t &c);
   void eval_restricted_update(cmd_t &c);
   void eval_move(cmd_t &c);
@@ -77,6 +87,7 @@ inline void computer_state_t::union_node_set(node_set_t& src, node_set_t& dst, b
       }
     } else if (src_t_v.second.size() != dst_t_v->second.size()) {
         std::cout << "error: union_node_set\n";
+        error = true;
     } else {
       for (unsigned i=0; i<src_t_v.second.size(); i++) {
         union_int_set(src_t_v.second.at(i), dst_t_v->second.at(i));
@@ -90,7 +101,87 @@ inline void computer_state_t::union_value(value_t& src, value_t& dst) {
   union_node_set(src.node_set, dst.node_set);
 }
 
+inline bool computer_state_t::eval_predicate_int(int32_t i, predicate_t& p) {
+  switch (p.type) {
+    case PRE_VALUE_IN:
+      return p.range.from <= i && i < p.range.to;
+      break;
+    case PRE_VALUE_NOT_IN:
+      return p.range.from > i || i >= p.range.to;
+      break;
+    default:
+      break;
+  }
+  return true;
+}
+
+inline bool computer_state_t::eval_predicate_tag(int32_t t, predicate_t& p) {
+  switch (p.type) {
+    case PRE_TAG_IN:
+      return prg.intset[p.tag_set_id].find(t) != prg.intset[p.tag_set_id].end();
+      break;
+    case PRE_TAG_NOT_IN:
+      return prg.intset[p.tag_set_id].find(t) == prg.intset[p.tag_set_id].end();
+      break;
+    default:
+      break;
+  }
+  return true;
+}
+
+inline void computer_state_t::conditional_union_int_set(std::unordered_set<int32_t>& src, std::unordered_set<int32_t>& dst, predicate_t& p) {
+  for (auto& src_i: src) {
+    if (eval_predicate_int(src_i, p)) {
+      insert_int_set(dst, src_i);
+    }
+  }
+}
+
+inline void computer_state_t::conditional_union_node_set(node_set_t& src, node_set_t& dst, predicate_t& p) {
+  for (auto& src_t_v: src) {
+
+    if (!eval_predicate_tag(src_t_v.first, p)) continue;
+
+    node_set_t::iterator dst_t_v = dst.find (src_t_v.first);
+
+    if ( dst_t_v == dst.end() ) {
+      // case A) new tag in dst node set
+
+      // create new vector of int sets
+      node_set_t::mapped_type dst_vector(src_t_v.second.size());
+
+      // fill int set vector accoding the predicate
+      for (unsigned i=0; i<src_t_v.second.size(); i++) {
+        conditional_union_int_set(src_t_v.second.at(i), dst_vector[i], p);
+      }
+      // add to dst
+      dst.insert({src_t_v.first, dst_vector});
+      changed = true;
+
+    } else if (src_t_v.second.size() != dst_t_v->second.size()) {
+        std::cout << "error: conditonal_union_node_set\n";
+        error = true;
+
+    } else {
+      // case B) existing tag in dst node set
+
+      for (unsigned i=0; i<src_t_v.second.size(); i++) {
+        conditional_union_int_set(src_t_v.second.at(i), dst_t_v->second.at(i), p);
+      }
+    }
+  }
+}
+
+inline void computer_state_t::conditional_union_value(value_t& src, value_t& dst, predicate_t& p) {
+  conditional_union_int_set(src.simple_type, dst.simple_type, p);
+  conditional_union_node_set(src.node_set, dst.node_set, p);
+}
+
 // command evaluation
+
+inline void computer_state_t::eval_conditional_move(cmd_t &c) {
+  conditional_union_value(reg[c.cmd_conditional_move.src_reg], reg[c.cmd_conditional_move.dst_reg], c.cmd_conditional_move.predicate);
+}
 
 inline void computer_state_t::eval_restricted_move(cmd_t &c) {
   /*
@@ -214,8 +305,7 @@ inline void computer_state_t::eval_cmd(cmd_t &c) {
       break;
 
     case CMD_CONDITIONAL_MOVE:
-      // TODO
-      //  predicate
+      eval_conditional_move(c);
       break;
 
     case CMD_FETCH:
