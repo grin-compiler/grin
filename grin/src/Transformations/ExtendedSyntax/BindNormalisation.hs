f63b38223944609a41c129fd58318bf49a2ebd20
anabra <peter.d.podlovics@gmail.com>
2019-10-15 23:27:25 +0200

    ES Trf/Base: BindNormalisation

---
+++ grin/src/Transformations/ExtendedSyntax/BindNormalisation.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE LambdaCase #-}
+module Transformations.ExtendedSyntax.BindNormalisation where
+
+import Data.Functor.Foldable as Foldable
+
+import Grin.ExtendedSyntax.Grin
+
+bindNormalisation :: Exp -> Exp
+bindNormalisation = hylo alg coalg where
+  alg :: ExpF Exp -> Exp
+  alg (SBlockF e) = e
+  alg e = embed e
+
+  coalg :: Exp -> ExpF Exp
+  coalg (EBind lhs1 pat1 rhs1)
+    | EBind lhs2 pat2 rhs2 <- rmBlocks lhs1
+    = SBlockF $ EBind lhs2 pat2 (EBind (SBlock rhs2) pat1 rhs1)
+  coalg e = project e
+
+  rmBlocks :: Exp -> Exp
+  rmBlocks (SBlock e) = rmBlocks e
+  rmBlocks e          = e
