de35a2ca6e028dfe2fc8b83652997d6f417a050b
anabra <peter.d.podlovics@gmail.com>
2019-10-15 21:39:54 +0200

    ES GRIN misc: fixed Statistics

--- grin/src/Grin/ExtendedSyntax/Statistics.hs
+++ grin/src/Grin/ExtendedSyntax/Statistics.hs
@@ -18,7 +18,7 @@ data Statistics = Statistics
   , sApp    :: !Int
   , sReturn :: !Int
   , sStore  :: !Int
-  , sFetchI :: !Int
+  , sFetch  :: !Int
   , sUpdate :: !Int
   , sBlock  :: !Int
   -- Alt
@@ -52,29 +52,28 @@ statistics :: Exp -> Statistics
 statistics = cata $ \case
   DefF fn ps   s -> s <> mempty { def = 1, vars = Set.singleton fn <> Set.fromList ps }
   -- Exp
-  EBindF l b r -> l <> r     <> mempty { eBind = 1, vars = foldNames Set.singleton b, tags = tagInVal b }
-  ECaseF v xs  -> mconcat xs <> mempty { eCase = 1, vars = foldNames Set.singleton v, tags = tagInVal v }
+  EBindF l b r -> l <> r     <> mempty { eBind = 1, vars = foldNames Set.singleton b, tags = tagInBPat b }
+  ECaseF s xs  -> mconcat xs <> mempty { eCase = 1, vars = Set.singleton s }
   -- Simple Expr
-  SAppF f ps   -> mempty { sApp    = 1, vars = Set.singleton f <> foldNames Set.singleton ps }
-  SReturnF v   -> mempty { sReturn = 1, vars = foldNames Set.singleton v, tags = tagInVal v }
-  SStoreF  v   -> mempty { sStore  = 1, vars = foldNames Set.singleton v, tags = tagInVal v }
-  SFetchIF v _ -> mempty { sFetchI = 1, vars = Set.singleton v }
-  SUpdateF v x -> mempty { sUpdate = 1, vars = Set.singleton v <> foldNames Set.singleton x, tags = tagInVal x }
+  SAppF f ps   -> mempty { sApp    = 1, vars = Set.singleton f <> foldMap Set.singleton ps }
+  SReturnF v   -> mempty { sReturn = 1, vars = foldNames Set.singleton v }
+  SStoreF  v   -> mempty { sStore  = 1, vars = Set.singleton v }
+  SFetchF  v   -> mempty { sFetch  = 1, vars = Set.singleton v }
+  SUpdateF p v -> mempty { sUpdate = 1, vars = Set.singleton p <> Set.singleton v }
   SBlockF  s   -> s <> mempty { sBlock  = 1 }
   -- Alt
   AltF p s     -> s <> mempty { alt = 1, vars = foldNames Set.singleton p, tags = tagInCPat p }
   -- general case
   e -> Data.Foldable.fold e
 
-tagInVal :: Val -> Set.Set Tag
-tagInVal = \case
-  ConstTagNode t _ -> Set.singleton t
-  _                -> Set.empty
+tagInBPat :: BPat -> Set.Set Tag
+tagInBPat (AsPat var (ConstTagNode t _)) = Set.singleton t
+tagInBPat _ = mempty
+
 
 tagInCPat :: CPat -> Set.Set Tag
 tagInCPat = \case
   NodePat t _ -> Set.singleton t
-  TagPat  t   -> Set.singleton t
   _           -> Set.empty
 
 instance Pretty Statistics where
@@ -87,11 +86,11 @@ instance Pretty Statistics where
     , hsep [yellow $ fill 16 $ text "Function calls:", green $ text $ show sApp]
     , hsep [yellow $ fill 16 $ text "Returns:", green $ text $ show sReturn]
     , hsep [yellow $ fill 16 $ text "Stores:", green $ text $ show sStore]
-    , hsep [yellow $ fill 16 $ text "Fethces:", green $ text $ show sFetchI]
+    , hsep [yellow $ fill 16 $ text "Fethces:", green $ text $ show sFetch]
     , hsep [yellow $ fill 16 $ text "Updates:", green $ text $ show sUpdate]
     , yellow $ text "------------------"
     , hsep [yellow $ fill 16 $ text "Node summary:", green $ text $ show $ sum
-              [def, eBind, eCase, alt, sApp, sReturn, sStore, sFetchI, sUpdate]]
+              [def, eBind, eCase, alt, sApp, sReturn, sStore, sFetch, sUpdate]]
     , text ""
     , hsep [yellow $ fill 16 $ text "Variables:", green $ text $ show $ Set.size vars]
     , hsep [yellow $ fill 16 $ text "Tags:", green $ text $ show $ Set.size tags]
