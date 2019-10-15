1fd34c7d32a2a0d8254fcefe8c2adac08aa2d92c
anabra <peter.d.podlovics@gmail.com>
2019-10-15 20:28:57 +0200

    ES GRIN misc: added Nametable, PrettyLint, Research and Statistics modules

---
+++ grin/src/Grin/ExtendedSyntax/Nametable.hs
@@ -0,0 +1,159 @@
+{-# LANGUAGE LambdaCase, TemplateHaskell, RecordWildCards #-}
+module Grin.ExtendedSyntax.Nametable
+  ( Nametable
+  , convert
+  , restore
+  ) where
+
+import Data.Text (Text, unpack)
+import Grin.ExtendedSyntax.Syntax
+import qualified Data.Map.Strict as Map
+import Data.Functor.Foldable
+import Control.Monad.State
+import Lens.Micro.Platform
+import Data.Bifunctor
+
+-- * Convert
+
+type Nametable = Map.Map Int Text
+type ReverseTable = Map.Map Text Int
+
+data NS = NS
+  { _latest       :: !Int
+  , _nametable    :: Nametable
+  , _reverseTable :: ReverseTable
+  }
+
+emptyNS :: NS
+emptyNS = NS 0 Map.empty Map.empty
+
+makeLenses ''NS
+
+type NametableM = State NS
+
+nameToIdx :: Name -> NametableM Name
+nameToIdx ni@NI{} = pure ni
+nameToIdx (NM n) = do
+  mIdx <- use (reverseTable . at n)
+  case mIdx of
+    Just i  -> pure $ NI i
+    Nothing -> do
+      idx <- latest <<%= succ -- Modifies the state and returns the old value
+      nametable       %= Map.insert idx n
+      reverseTable    %= Map.insert n idx
+      pure $ NI idx
+
+tag :: Tag -> NametableM Tag
+tag (Tag tt tn) = Tag tt <$> nameToIdx tn
+
+lit :: Lit -> NametableM Lit
+lit = pure -- TODO: Handle string literals
+
+value :: Val -> NametableM Val
+value = \case
+  ConstTagNode t vs -> ConstTagNode <$> tag t <*> mapM value vs
+  VarTagNode   n vs -> VarTagNode <$> nameToIdx n <*> mapM value vs
+  ValTag       t    -> ValTag <$> tag t
+  Unit              -> pure Unit
+  Lit l             -> Lit <$> lit l
+  Var n             -> Var <$> nameToIdx n
+  Undefined ty      -> pure $ Undefined ty
+
+cpat :: CPat -> NametableM CPat
+cpat = \case
+  NodePat t ns -> NodePat <$> tag t <*> mapM nameToIdx ns
+  LitPat  l    -> LitPat <$> lit l
+  DefaultPat   -> pure DefaultPat
+  TagPat  t    -> TagPat <$> tag t
+
+ty :: Ty -> NametableM Ty
+ty = \case
+  TyCon    n ts -> TyCon <$> nameToIdx n <*> mapM ty ts
+  TyVar    n    -> TyVar <$> nameToIdx n
+  TySimple st   -> pure $ TySimple st
+
+external :: External -> NametableM External
+external (External{..}) =
+  External <$> nameToIdx eName
+           <*> ty eRetType
+           <*> mapM ty eArgsType
+           <*> (pure eEffectful)
+           <*> (pure eKind)
+
+-- | Convert Names in the expression to Int identifiers and create
+-- an associated name table.
+convert :: Exp -> (Exp, Nametable)
+convert = second (view nametable) . flip runState emptyNS . cata build where
+  build :: ExpF (NametableM Exp) -> NametableM Exp
+  build = \case
+    ProgramF es defs  -> Program <$> mapM external es <*> sequence defs
+    DefF fn ps body   -> Def <$> (nameToIdx fn) <*> (mapM nameToIdx ps) <*> body
+    EBindF l v r      -> EBind <$> l <*> value v <*> r
+    ECaseF v alts     -> ECase <$> value v <*> sequence alts
+    SAppF v ps        -> SApp <$> nameToIdx v <*> (mapM value ps)
+    SReturnF v        -> SReturn <$> value v
+    SStoreF v         -> SStore <$> value v
+    SFetchIF n p      -> SFetchI <$> nameToIdx n <*> (pure p)
+    SUpdateF n v      -> SUpdate <$> nameToIdx n <*> value v
+    SBlockF body      -> SBlock <$> body
+    AltF cp e         -> Alt <$> cpat cp <*> e
+
+-- * Restore
+
+-- | Restore names from a nametable, assuming that all the
+-- identifiers are present in the table.
+restore :: (Exp, Nametable) -> Exp
+restore (exp, nt) = cata build exp where
+  build :: ExpF Exp -> Exp
+  build = \case
+    ProgramF es defs  -> Program (map rexternal es) defs
+    DefF fn ps body   -> Def (rname fn) (map rname ps) body
+    EBindF l v r      -> EBind l (rvalue v) r
+    ECaseF v alts     -> ECase (rvalue v) alts
+    SAppF v ps        -> SApp (rname v) (map rvalue ps)
+    SReturnF v        -> SReturn (rvalue v)
+    SStoreF v         -> SStore (rvalue v)
+    SFetchIF n p      -> SFetchI (rname n) p
+    SUpdateF n v      -> SUpdate (rname n) (rvalue v)
+    SBlockF body      -> SBlock body
+    AltF cp e         -> Alt (rcpat cp) e
+
+  rname :: Name -> Name
+  rname (NI i) = maybe (error $ show i ++ " is not found") NM $ Map.lookup i nt
+
+  rvalue :: Val -> Val
+  rvalue = \case
+    ConstTagNode t vs -> ConstTagNode (rtag t) (map rvalue vs)
+    VarTagNode   n vs -> VarTagNode (rname n) (map rvalue vs)
+    ValTag       t    -> ValTag (rtag t)
+    Unit              -> Unit
+    Lit l             -> Lit (rlit l)
+    Var n             -> Var (rname n)
+    Undefined ty      -> Undefined ty
+
+  rlit :: Lit -> Lit
+  rlit = id -- TODO: Handle String literals
+
+  rtag :: Tag -> Tag
+  rtag (Tag tt tn) = Tag tt (rname tn)
+
+  rcpat :: CPat -> CPat
+  rcpat = \case
+    NodePat t ns -> NodePat (rtag t) (map rname ns)
+    LitPat  l    -> LitPat (rlit l)
+    DefaultPat   -> DefaultPat
+    TagPat  t    -> TagPat (rtag t)
+
+  rexternal :: External -> External
+  rexternal External{..} =
+    External (rname eName)
+             (rty eRetType)
+             (map rty eArgsType)
+             eEffectful
+             eKind
+
+  rty :: Ty -> Ty
+  rty = \case
+    TyCon    n ts -> TyCon (rname n) (map rty ts)
+    TyVar    n    -> TyVar (rname n)
+    TySimple st   -> TySimple st
---
+++ grin/src/Grin/ExtendedSyntax/Statistics.hs
@@ -0,0 +1,98 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RecordWildCards #-}
+module Grin.ExtendedSyntax.Statistics where
+
+import Data.Monoid
+import Data.Functor.Foldable
+import qualified Data.Foldable
+import Text.PrettyPrint.ANSI.Leijen
+import Grin.ExtendedSyntax.Grin
+import qualified Data.Set as Set
+
+
+data Statistics = Statistics
+  { def     :: !Int
+  , eBind   :: !Int
+  , eCase   :: !Int
+  -- Simple Exp
+  , sApp    :: !Int
+  , sReturn :: !Int
+  , sStore  :: !Int
+  , sFetchI :: !Int
+  , sUpdate :: !Int
+  , sBlock  :: !Int
+  -- Alt
+  , alt     :: !Int
+  , vars    :: !(Set.Set Name)
+  , tags    :: !(Set.Set Tag)
+  } deriving (Show)
+
+instance Monoid Statistics where
+  mempty = Statistics 0 0 0 0 0 0 0 0 0 0 mempty mempty
+
+instance Semigroup Statistics where
+  (<>)
+    (Statistics a0 b0 c0 d0 e0 f0 g0 h0 i0 j0 k0 l0)
+    (Statistics a1 b1 c1 d1 e1 f1 g1 h1 i1 j1 k1 l1)
+    = Statistics
+        (a0 + a1)
+        (b0 + b1)
+        (c0 + c1)
+        (d0 + d1)
+        (e0 + e1)
+        (f0 + f1)
+        (g0 + g1)
+        (h0 + h1)
+        (i0 + i1)
+        (j0 + j1)
+        (k0 <> k1)
+        (l0 <> l1)
+
+statistics :: Exp -> Statistics
+statistics = cata $ \case
+  DefF fn ps   s -> s <> mempty { def = 1, vars = Set.singleton fn <> Set.fromList ps }
+  -- Exp
+  EBindF l b r -> l <> r     <> mempty { eBind = 1, vars = foldNames Set.singleton b, tags = tagInVal b }
+  ECaseF v xs  -> mconcat xs <> mempty { eCase = 1, vars = foldNames Set.singleton v, tags = tagInVal v }
+  -- Simple Expr
+  SAppF f ps   -> mempty { sApp    = 1, vars = Set.singleton f <> foldNames Set.singleton ps }
+  SReturnF v   -> mempty { sReturn = 1, vars = foldNames Set.singleton v, tags = tagInVal v }
+  SStoreF  v   -> mempty { sStore  = 1, vars = foldNames Set.singleton v, tags = tagInVal v }
+  SFetchIF v _ -> mempty { sFetchI = 1, vars = Set.singleton v }
+  SUpdateF v x -> mempty { sUpdate = 1, vars = Set.singleton v <> foldNames Set.singleton x, tags = tagInVal x }
+  SBlockF  s   -> s <> mempty { sBlock  = 1 }
+  -- Alt
+  AltF p s     -> s <> mempty { alt = 1, vars = foldNames Set.singleton p, tags = tagInCPat p }
+  -- general case
+  e -> Data.Foldable.fold e
+
+tagInVal :: Val -> Set.Set Tag
+tagInVal = \case
+  ConstTagNode t _ -> Set.singleton t
+  _                -> Set.empty
+
+tagInCPat :: CPat -> Set.Set Tag
+tagInCPat = \case
+  NodePat t _ -> Set.singleton t
+  TagPat  t   -> Set.singleton t
+  _           -> Set.empty
+
+instance Pretty Statistics where
+  pretty (Statistics{..}) = vsep
+    [ hsep [yellow $ fill 16 $ text "Definitions:", green $ text $ show def]
+    , hsep [yellow $ fill 16 $ text "Binds:", green $ text $ show eBind]
+    , hsep [yellow $ fill 16 $ text "Blocks:", green $ text $ show sBlock]
+    , hsep [yellow $ fill 16 $ text "Cases:", green $ text $ show eCase]
+    , hsep [yellow $ fill 16 $ text "Alternatives:", green $ text $ show alt]
+    , hsep [yellow $ fill 16 $ text "Function calls:", green $ text $ show sApp]
+    , hsep [yellow $ fill 16 $ text "Returns:", green $ text $ show sReturn]
+    , hsep [yellow $ fill 16 $ text "Stores:", green $ text $ show sStore]
+    , hsep [yellow $ fill 16 $ text "Fethces:", green $ text $ show sFetchI]
+    , hsep [yellow $ fill 16 $ text "Updates:", green $ text $ show sUpdate]
+    , yellow $ text "------------------"
+    , hsep [yellow $ fill 16 $ text "Node summary:", green $ text $ show $ sum
+              [def, eBind, eCase, alt, sApp, sReturn, sStore, sFetchI, sUpdate]]
+    , text ""
+    , hsep [yellow $ fill 16 $ text "Variables:", green $ text $ show $ Set.size vars]
+    , hsep [yellow $ fill 16 $ text "Tags:", green $ text $ show $ Set.size tags]
+    ]
