16fc9ee5365b1e7b53d80d0c08d5a1fa03bd8d3e
Luc Tielen <luc.tielen@kabisa.nl>
2019-12-08 17:52:32 +0100

    Provide cabal and ghcid when in nix-shell

--- shell.nix
+++ shell.nix
@@ -2,11 +2,13 @@
 
 let
   compiler = "ghc865";
+  hpkg = pkgs.haskell.packages."${compiler}";
   grin = import ./. { inherit compiler pkgs; };
 in
 
-pkgs.haskell.packages."${compiler}".shellFor {
+hpkg.shellFor {
   withHoogle = true;
   packages = p: [ grin ];
+  buildInputs = with hpkg; [ cabal-install ghcid ];
   inherit (grin.env) nativeBuildInputs;
 }
