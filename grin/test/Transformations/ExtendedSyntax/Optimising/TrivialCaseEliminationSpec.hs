ec365a24eda16a7aaa9fc8fe1297ba218f4adbf7
anabra <peter.d.podlovics@gmail.com>
2019-12-16 22:57:45 +0100

    ES: added tests for TrivialCaseElimination

--- grin/test/Transformations/ExtendedSyntax/Optimising/TrivialCaseEliminationSpec.hs
+++ grin/test/Transformations/ExtendedSyntax/Optimising/TrivialCaseEliminationSpec.hs
@@ -1,12 +1,12 @@
 {-# LANGUAGE OverloadedStrings, QuasiQuotes, ViewPatterns #-}
 module Transformations.ExtendedSyntax.Optimising.TrivialCaseEliminationSpec where
 
-import Transformations.Optimising.TrivialCaseElimination
+import Transformations.ExtendedSyntax.Optimising.TrivialCaseElimination
 
 import Test.Hspec
-import Grin.TH
-import Test.Test hiding (newVar)
-import Test.Assertions
+import Grin.ExtendedSyntax.TH
+import Test.ExtendedSyntax.New.Test hiding (newVar)
+import Test.ExtendedSyntax.Assertions
 
 
 runTests :: IO ()
@@ -18,11 +18,11 @@ spec = do
     it "Figure 4.24" $ do
       let before = [expr|
           case v of
-            (Ffun a1 a2 a3) -> fun a1 a2 a3
+            (Ffun a1 a2 a3) @ alt1 -> fun a1 a2 a3
         |]
       let after = [expr|
           do
-            (Ffun a1 a2 a3) <- pure v
+            (Ffun a1 a2 a3) @ alt1 <- pure v
             fun a1 a2 a3
         |]
       trivialCaseElimination (ctx before) `sameAs` (ctx after)
@@ -30,23 +30,24 @@ spec = do
     it "bypass" $ do
       let before = [expr|
           case v of
-            (Ffun1 a1 a2 a3) -> fun1 a1 a2 a3
-            #default -> pure 2
+            (Ffun1 a1 a2 a3) @ alt1 -> fun1 a1 a2 a3
+            #default         @ alt2 -> pure 2
         |]
       let after = [expr|
           case v of
-            (Ffun1 a1 a2 a3) -> fun1 a1 a2 a3
-            #default -> pure 2
+            (Ffun1 a1 a2 a3) @ alt1 -> fun1 a1 a2 a3
+            #default         @ alt2 -> pure 2
         |]
       trivialCaseElimination (ctx before) `sameAs` (ctx after)
 
     it "default alternative" $ do
       let before = [expr|
           case v of
-            #default -> pure 2
+            #default @ alt1 -> pure 2
         |]
       let after = [expr|
           do
+            alt1 <- pure v
             pure 2
         |]
       trivialCaseElimination (ctx before) `sameAs` (ctx after)
