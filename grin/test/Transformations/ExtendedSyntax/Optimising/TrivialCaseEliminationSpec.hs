8c39025e8066b6b0b3dc18caf1cdc954740eef73
anabra <peter.d.podlovics@gmail.com>
2019-12-16 21:58:20 +0100

    ES: added tests for Evaluated and Trivial Case Elimination

---
+++ grin/test/Transformations/ExtendedSyntax/Optimising/TrivialCaseEliminationSpec.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE OverloadedStrings, QuasiQuotes, ViewPatterns #-}
+module Transformations.ExtendedSyntax.Optimising.TrivialCaseEliminationSpec where
+
+import Transformations.Optimising.TrivialCaseElimination
+
+import Test.Hspec
+import Grin.TH
+import Test.Test hiding (newVar)
+import Test.Assertions
+
+
+runTests :: IO ()
+runTests = hspec spec
+
+spec :: Spec
+spec = do
+  testExprContextE $ \ctx -> do
+    it "Figure 4.24" $ do
+      let before = [expr|
+          case v of
+            (Ffun a1 a2 a3) -> fun a1 a2 a3
+        |]
+      let after = [expr|
+          do
+            (Ffun a1 a2 a3) <- pure v
+            fun a1 a2 a3
+        |]
+      trivialCaseElimination (ctx before) `sameAs` (ctx after)
+
+    it "bypass" $ do
+      let before = [expr|
+          case v of
+            (Ffun1 a1 a2 a3) -> fun1 a1 a2 a3
+            #default -> pure 2
+        |]
+      let after = [expr|
+          case v of
+            (Ffun1 a1 a2 a3) -> fun1 a1 a2 a3
+            #default -> pure 2
+        |]
+      trivialCaseElimination (ctx before) `sameAs` (ctx after)
+
+    it "default alternative" $ do
+      let before = [expr|
+          case v of
+            #default -> pure 2
+        |]
+      let after = [expr|
+          do
+            pure 2
+        |]
+      trivialCaseElimination (ctx before) `sameAs` (ctx after)
