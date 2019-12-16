ad2c8887606e362bdf2237f7ddea2bee44e7ca8b
anabra <peter.d.podlovics@gmail.com>
2019-12-16 21:58:20 +0100

    ES: added tests for Evaluated and Trivial Case Elimination

---
+++ grin/test/Transformations/ExtendedSyntax/Optimising/EvaluatedCaseEliminationSpec.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE OverloadedStrings, QuasiQuotes, ViewPatterns #-}
+module Transformations.ExtendedSyntax.Optimising.EvaluatedCaseEliminationSpec where
+
+import Transformations.Optimising.EvaluatedCaseElimination
+
+import Test.Hspec
+import Grin.TH
+import Test.Test hiding (newVar)
+import Test.Assertions
+
+
+runTests :: IO ()
+runTests = hspec spec
+
+spec :: Spec
+spec = do
+  testExprContextE $ \ctx -> do
+    it "Figure 4.22" $ do
+      let before = [expr|
+          case v of
+            (CLeft l)  -> pure v
+            (CRight r) -> pure v
+        |]
+      let after = [expr|
+          pure v
+        |]
+      evaluatedCaseElimination (ctx before) `sameAs` (ctx after)
+
+    it "default case" $ do
+      let before = [expr|
+          case v of
+            (CLeft l)   -> pure v
+            #default    -> pure v
+        |]
+      let after = [expr|
+          pure v
+        |]
+      evaluatedCaseElimination (ctx before) `sameAs` (ctx after)
