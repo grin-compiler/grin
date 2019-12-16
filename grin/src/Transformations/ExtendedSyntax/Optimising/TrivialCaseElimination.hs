252cf1405fde44d67debec9c7ae8bd5c2b028ade
anabra <peter.d.podlovics@gmail.com>
2019-12-16 21:34:09 +0100

    ES: added Evaluated and Trivial Case Elimination

---
+++ grin/src/Transformations/ExtendedSyntax/Optimising/TrivialCaseElimination.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE LambdaCase, TupleSections #-}
+module Transformations.ExtendedSyntax.Optimising.TrivialCaseElimination where
+
+import Data.Functor.Foldable as Foldable
+import Grin.Grin
+import Transformations.Util
+
+trivialCaseElimination :: Exp -> Exp
+trivialCaseElimination = ana builder where
+  builder :: Exp -> ExpF Exp
+  builder = \case
+    ECase val [Alt DefaultPat body] -> SBlockF body
+    ECase val [Alt cpat body]       -> SBlockF $ EBind (SReturn val)  (cpatToLPat cpat) body
+    exp -> project exp
