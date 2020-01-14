e71972cd3ccf7e95f1c00b803f316c3108daf0b6
anabra <peter.d.podlovics@gmail.com>
2020-01-14 23:46:51 +0100

    ES: added tests for NonSharedElimination

---
+++ grin/test/Transformations/ExtendedSyntax/Optimising/NonSharedEliminationSpec.hs
@@ -0,0 +1,86 @@
+{-# LANGUAGE OverloadedStrings, QuasiQuotes, ViewPatterns #-}
+module Transformations.ExtendedSyntax.Optimising.NonSharedEliminationSpec where
+
+import Transformations.ExtendedSyntax.Optimising.NonSharedElimination
+
+import qualified Data.Set as Set
+import qualified Data.Map as Map
+
+import Test.Hspec
+import Test.ExtendedSyntax.Util (loc)
+import Test.ExtendedSyntax.Assertions
+import Test.ExtendedSyntax.New.Test (testExprContextE)
+
+import Grin.ExtendedSyntax.Syntax
+import AbstractInterpretation.ExtendedSyntax.HeapPointsTo.Result as HPT
+import AbstractInterpretation.ExtendedSyntax.Sharing.Result
+import Grin.ExtendedSyntax.TH (expr)
+
+
+runTests :: IO ()
+runTests = hspec spec
+
+nonSharedElimination' :: SharingResult -> Exp -> Exp
+nonSharedElimination' shRes = fst . nonSharedElimination shRes
+
+-- NOTE: The type environments are partial, because we only need pointer information.
+
+spec :: Spec
+spec = do
+  testExprContextE $ \ctx -> do
+    it "simple non-shared" $ do
+      let before = [expr|
+          n1 <- pure (COne)
+          p1 <- store n1
+          v1 <- fetch p1
+          n2 <- pure (CTwo)
+          _1 <- update p1 n2
+          pure ()
+        |]
+      let after = [expr|
+          n1 <- pure (COne)
+          p1 <- store n1
+          v1 <- fetch p1
+          n2 <- pure (CTwo)
+          pure ()
+        |]
+
+      let hptResult = HPTResult
+            { HPT._memory   = mempty
+            , HPT._register = Map.fromList [ ("p1",   loc 0)]
+            , HPT._function = mempty
+            }
+          sharedLocs = mempty
+          shResult = SharingResult hptResult sharedLocs
+
+      nonSharedElimination' shResult (ctx before) `sameAs` (ctx after)
+
+    it "simple shared" $ do
+      let before = [expr|
+          n1 <- pure (COne)
+          p1 <- store n1
+          v1 <- fetch p1
+          n2 <- pure (CTwo)
+          _1 <- update p1 n2
+          v2 <- fetch p1
+          pure ()
+        |]
+      let after = [expr|
+          n1 <- pure (COne)
+          p1 <- store n1
+          v1 <- fetch p1
+          n2 <- pure (CTwo)
+          _1 <- update p1 n2
+          v2 <- fetch p1
+          pure ()
+        |]
+
+      let hptResult = HPTResult
+            { HPT._memory   = mempty
+            , HPT._register = Map.fromList [ ("p1",   loc 0)]
+            , HPT._function = mempty
+            }
+          sharedLocs = Set.fromList [0]
+          shResult = SharingResult hptResult sharedLocs
+
+      nonSharedElimination' shResult (ctx before) `sameAs` (ctx after)
