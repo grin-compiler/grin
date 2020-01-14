6666a18477527ce07a8a49305eed9d00754e2537
anabra <peter.d.podlovics@gmail.com>
2020-01-14 23:46:51 +0100

    ES: added tests for NonSharedElimination

--- grin/src/Transformations/ExtendedSyntax/Optimising/NonSharedElimination.hs
+++ grin/src/Transformations/ExtendedSyntax/Optimising/NonSharedElimination.hs
@@ -5,31 +5,31 @@ module Transformations.ExtendedSyntax.Optimising.NonSharedElimination where
 Remove the updates that update only non-shared locations.
 -}
 
-import Data.Maybe
 import qualified Data.Set as Set
 import Data.Functor.Foldable as Foldable
 
-import Lens.Micro
-
 import Grin.ExtendedSyntax.Grin
-import Grin.ExtendedSyntax.TypeEnv
+import Grin.ExtendedSyntax.TypeEnv (ptrLocations)
+import Grin.ExtendedSyntax.TypeCheck (typeEnvFromHPTResult)
 import Transformations.ExtendedSyntax.Names (ExpChanges(..))
-import AbstractInterpretation.ExtendedSyntax.Sharing.Result
+import AbstractInterpretation.ExtendedSyntax.Sharing.Result (SharingResult(..))
 
 
 
-nonSharedElimination :: SharingResult -> TypeEnv -> Exp -> (Exp, ExpChanges)
-nonSharedElimination SharingResult{..} te exp = (exp', change) where
+nonSharedElimination :: SharingResult -> Exp -> (Exp, ExpChanges)
+nonSharedElimination SharingResult{..} exp = (exp', change) where
 
   exp' = cata skipUpdate exp
 
   change = if exp' /= exp then DeletedHeapOperation else NoChange
 
+  tyEnv = either error id $ typeEnvFromHPTResult _hptResult
+
   -- Remove bind when the parameter points to non-shared locations only.
   skipUpdate :: ExpF Exp -> Exp
   skipUpdate = \case
     EBindF (SUpdate p _) _ rhs
-      | all notShared . ptrLocations te $ p -> rhs
+      | all notShared . ptrLocations tyEnv $ p -> rhs
     exp -> embed exp
 
   notShared :: Loc -> Bool
