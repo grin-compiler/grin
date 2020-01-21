c8af56e8304373e5baed2a66bbe8a90698149189
anabra <peter.d.podlovics@gmail.com>
2020-01-21 14:08:58 +0100

    ES: added CCP and tests

---
+++ grin/src/Transformations/ExtendedSyntax/Optimising/CaseCopyPropagation.hs
@@ -0,0 +1,84 @@
+{-# LANGUAGE LambdaCase, OverloadedStrings #-}
+module Transformations.ExtendedSyntax.Optimising.CaseCopyPropagation where
+
+import Grin.Grin
+import Grin.TypeEnv
+import Data.Maybe
+import qualified Data.Set as Set
+import qualified Data.Map as Map ( fromList, union )
+import Control.Monad.Trans.State
+import Control.Monad.Trans ( lift )
+import Data.Bifunctor ( first )
+
+import Data.Functor.Foldable
+import Transformations.Names
+import Transformations.Util
+
+
+
+
+caseCopyPropagation :: Exp -> (Exp, ExpChanges)
+caseCopyPropagation exp0 = first neExp $ evalNameM exp0 $ paraM folder exp0
+
+data Changes = None | InChange Tag | Final deriving (Eq, Show)
+
+data NewExp = NE
+  { neChanges :: Changes
+  , neExp     :: Exp
+  } deriving Show
+
+folder :: ExpF (Exp, NewExp) -> NameM NewExp
+folder = \case
+  ProgramF exts ds                      -> pure $ NE Final (Program exts $ map (neExp . snd) ds)
+  DefF n ps body@(b, NE (InChange _) _) -> pure $ NE Final (Def n ps b)
+  DefF n ps body@(_, NE c e)            -> pure $ NE c (Def n ps e)
+
+  SReturnF (ConstTagNode tag [value]) -> pure $ NE (InChange tag) (SReturn value)
+
+  SBlockF (b, NE (InChange _) _) -> pure $ NE None (SBlock b)
+  SBlockF (_, NE c e)            -> pure $ NE c    (SBlock e)
+
+  exp | isPrimitiveExp (embed $ fmap fst exp) -> pure $ NE None (embed $ fmap (neExp . snd) exp)
+
+  EBindF (_, NE None  lhse) pat (_, NE None rhse)                -> pure $ NE None (EBind lhse pat rhse)
+  EBindF (_, NE None  lhse) pat (_, NE (InChange t) rhse)        -> pure $ NE (InChange t) (EBind lhse pat rhse)
+  EBindF (_, NE None  lhse) pat (_, NE Final rhse)               -> pure $ NE Final (EBind lhse pat rhse)
+  EBindF (_, NE Final lhse) pat (_, NE None  rhse)               -> pure $ NE Final (EBind lhse pat rhse)
+  EBindF (_, NE Final lhse) pat (rhso, NE (InChange _) _)        -> pure $ NE Final (EBind lhse pat rhso)
+  EBindF (_, NE Final lhse) pat (_, NE Final rhse)               -> pure $ NE Final (EBind lhse pat rhse)
+  EBindF (lhso, _) pat (_, NE change rhse) | isPrimitiveExp lhso -> pure $ NE change (EBind lhso pat rhse)
+
+  AltF cpat body@(_, NE c e) -> pure $ NE c (Alt cpat e)
+
+  exp@(ECaseF val alts)
+    -- Each alternative has the same tag and they are changed.
+    | tags <- map (neTag . snd) alts, all isJust tags
+      -> if allSame tags
+          then do var <- deriveNewName "ccp"
+                  let (Just tag) = neTag $ snd $ head alts
+                  pure $ NE
+                    Final
+                    (SBlock (EBind
+                        (ECase val (map (neExp . snd) alts))
+                        (Var var)
+                        (SReturn (ConstTagNode tag [Var var]))))
+          else pure $ NE None (ECase val (map fst alts))
+    -- Some of the alternatives are final, thus we mark this case as Final and we don't change it.
+    | any (hasChange Final) alts
+        -> pure $ NE Final (ECase val $ map getFinalExp alts)
+    -- Nothing has changed in the alternatives.
+    | all (hasChange None) alts
+        -> pure $ NE None (ECase val $ map (neExp . snd) alts)
+    -- Some of the alternatives could change, some none, thus we leave them unchanged.
+    | any (isJust . neTag . snd) alts
+        -> pure $ NE None (ECase val $ map fst alts)
+  where
+    neTag (NE (InChange t)_ ) = Just t
+    neTag _                   = Nothing
+
+    hasChange c0 (_, NE c _) = c == c0
+    getFinalExp (oe, NE c ne) = case c of { Final -> ne; _ -> oe }
+
+allSame :: (Eq a) => [a] -> Bool
+allSame [] = False
+allSame (x:xs) = all (x ==) xs
---
+++ grin/test/Transformations/ExtendedSyntax/Optimising/CaseCopyPropagationSpec.hs
@@ -0,0 +1,269 @@
+{-# LANGUAGE OverloadedStrings, QuasiQuotes #-}
+module Transformations.ExtendedSyntax.Optimising.CaseCopyPropagationSpec where
+
+import Transformations.ExtendedSyntax.Optimising.CaseCopyPropagation
+import Transformations.Names (ExpChanges(..))
+
+import Test.Hspec
+import Grin.TH
+import Test.Test hiding (newVar)
+import Test.Assertions
+import Grin.TypeEnv
+import Data.Monoid
+import Grin.Pretty
+
+ctxs :: [TestExpContext]
+ctxs =
+  [ emptyCtx
+  , lastBindR
+  , firstAlt
+  , middleAlt
+--  , lastAlt
+  ]
+
+--spec :: Spec
+--spec = pure ()
+
+
+-- TODO: Check parsing.
+spec :: Spec
+spec = testExprContextIn ctxs $ \ctx -> do
+
+  it "Example from Figure 4.26" $ do
+    let teBefore = create $
+          (newVar "z'" int64_t) <>
+          (newVar "y'" int64_t) <>
+          (newVar "x'" int64_t)
+    let before = [expr|
+            m0 <- store (CNone)
+            u  <- case v of
+                    (Ffoo a)  -> y' <- foo a
+                                 pure (CInt y')
+                    (Fbar b)  -> z' <- bar b
+                                 pure (CInt z')
+                    (CInt x') -> pure (CInt x')
+            pure m0
+          |]
+
+    let teAfter = extend teBefore $
+          newVar "v'" int64_t
+    let after = [expr|
+            m0 <- store (CNone)
+            u  <- do
+              ccp.0 <- case v of
+                  (Ffoo a)  -> y' <- foo a
+                               pure y'
+                  (Fbar b)  -> z' <- bar b
+                               pure z'
+                  (CInt x') -> pure x'
+              pure (CInt ccp.0)
+            pure m0
+          |]
+    -- TODO: Inspect type env
+    (caseCopyPropagation (snd (ctx (teBefore, before)))) `sameAs` ((snd (ctx (teAfter, after))), NewNames)
+    --(snd (ctx (teBefore, before))) `sameAs` (snd (ctx (teAfter, after)))
+
+  it "One node has no Int tagged value" $ do
+    let typeEnv = emptyTypeEnv
+    let teBefore = create $
+          (newVar "z'" float_t) <>
+          (newVar "y'" int64_t) <>
+          (newVar "x'" int64_t)
+    let before = [expr|
+            m0 <- store (CNone)
+            u  <- do
+              case v of
+                (Ffoo a)  -> y' <- foo a
+                             pure (CInt y')
+                (Fbar b)  -> z' <- bar b
+                             pure (CFloat z')
+                (CInt x') -> pure (CInt x')
+            pure m0
+          |]
+    let after = [expr|
+            m0 <- store (CNone)
+            u  <- do
+              case v of
+                (Ffoo a)  -> y' <- foo a
+                             pure (CInt y')
+                (Fbar b)  -> z' <- bar b
+                             pure (CFloat z')
+                (CInt x') -> pure (CInt x')
+            pure m0
+          |]
+    (caseCopyPropagation (snd (ctx (teBefore, before)))) `sameAs` ((snd (ctx (teBefore, after))), NoChange)
+
+  xit "Embedded good case" $ do
+    let teBefore = create $
+          (newVar "z'" int64_t) <>
+          (newVar "y'" int64_t) <>
+          (newVar "x'" int64_t) <>
+          (newVar "z1'" int64_t) <>
+          (newVar "y1'" int64_t) <>
+          (newVar "x1'" int64_t)
+    let before = [expr|
+            m0 <- store (CNone)
+            u  <- case v of
+                    (Ffoo a)  -> y' <- foo a
+                                 pure (CInt y')
+                    (Fbar b)  -> z' <- bar b
+                                 pure (CInt z')
+                    (CInt x') -> u1 <- case v1 of
+                                         (Ffoo a1)  -> y1' <- foo a1
+                                                       pure (CInt y1')
+                                         (Fbar b1)  -> z1' <- bar b1
+                                                       pure (CInt z1')
+                                         (CInt x1') -> pure (CInt x1')
+                                 pure (CInt x')
+            pure m0
+          |]
+    let teAfter = extend teBefore $
+          newVar "v'" int64_t <>
+          newVar "v1'" int64_t
+    let after = [expr|
+            m0 <- store (CNone)
+            u  <- case v of
+                    (Ffoo a)  -> y' <- foo a
+                                 pure (CInt y')
+                    (Fbar b)  -> z' <- bar b
+                                 pure (CInt z')
+                    (CInt x') -> u1 <- do
+                                    ccp.0 <- case v1 of
+                                              (Ffoo a1)  -> y1' <- foo a1
+                                                            pure y1'
+                                              (Fbar b1)  -> z1' <- bar b1
+                                                            pure z1'
+                                              (CInt x1') -> pure x1'
+                                    pure (CInt ccp.0)
+                                  pure (CInt x')
+            pure m0
+          |]
+    (caseCopyPropagation (snd (ctx (teBefore, before)))) `sameAs` ((snd (ctx (teAfter, after))), NewNames)
+
+  it "Embedded bad case" $ do
+    let teBefore = create $
+          newVar "z'" int64_t <>
+          newVar "y'" int64_t <>
+          newVar "x'" int64_t <>
+          newVar "y1'" int64_t <>
+          newVar "z1'" float_t <>
+          newVar "x1'" int64_t
+    let before = [expr|
+            m0 <- store (CNone)
+            u  <- case v of
+                    (Ffoo a)  -> y' <- foo a
+                                 pure (CInt y')
+                    (Fbar b)  -> z' <- bar b
+                                 pure (CInt z')
+                    (CInt x') -> u1 <- do
+                                   case v1 of
+                                     (Ffoo a1)  -> y1' <- foo a1
+                                                   pure (CInt y1')
+                                     (Fbar b1)  -> z1' <- bar b1
+                                                   pure (CFloat z1')
+                                     (CInt x1') -> pure (CInt x1')
+                                 pure (CInt x')
+            pure m0
+          |]
+    let teAfter = extend teBefore $
+          newVar "v'" int64_t
+    let after = [expr|
+            m0 <- store (CNone)
+            u  <- do
+              ccp.0 <- case v of
+                        (Ffoo a)  -> y' <- foo a
+                                     pure y'
+                        (Fbar b)  -> z' <- bar b
+                                     pure z'
+                        (CInt x') -> u1 <- do
+                                       case v1 of
+                                         (Ffoo a1)  -> y1' <- foo a1
+                                                       pure (CInt y1')
+                                         (Fbar b1)  -> z1' <- bar b1
+                                                       pure (CFloat z1')
+                                         (CInt x1') -> pure (CInt x1')
+                                     pure x'
+              pure (CInt ccp.0)
+            pure m0
+          |]
+    (caseCopyPropagation (snd (ctx (teBefore, before)))) `sameAs` ((snd (ctx (teAfter, after))), NewNames)
+
+  it "Leave the outher, transform the inner" $ do
+    let teBefore = create $
+          newVar "z'" float_t <>
+          newVar "y'" int64_t <>
+          newVar "x'" int64_t <>
+          newVar "y1'" int64_t <>
+          newVar "z1'" int64_t <>
+          newVar "x1'" int64_t
+    let before = [expr|
+            m0 <- store (CNone)
+            u  <- do
+              case v of
+                (Ffoo a)  -> y' <- foo a
+                             pure (CInt y')
+                (Fbar b)  -> z' <- bar b
+                             pure (CFloat z')
+                (CInt x') -> u1 <- case v1 of
+                                     (Ffoo a1)  -> y1' <- foo a1
+                                                   pure (CInt y1')
+                                     (Fbar b1)  -> z1' <- bar b1
+                                                   pure (CInt z1')
+                                     (CInt x1') -> pure (CInt x1')
+                             pure (CInt x')
+            pure m0
+          |]
+    let teAfter = extend teBefore $
+          newVar "v1'" int64_t
+    let after = [expr|
+            m0 <- store (CNone)
+            u  <- do
+              case v of
+                (Ffoo a)  -> y' <- foo a
+                             pure (CInt y')
+                (Fbar b)  -> z' <- bar b
+                             pure (CFloat z')
+                (CInt x') -> u1 <- do
+                               ccp.0 <- case v1 of
+                                         (Ffoo a1)  -> y1' <- foo a1
+                                                       pure y1'
+                                         (Fbar b1)  -> z1' <- bar b1
+                                                       pure z1'
+                                         (CInt x1') -> pure x1'
+                               pure (CInt ccp.0)
+                             pure (CInt x')
+            pure m0
+          |]
+
+    (caseCopyPropagation (snd (ctx (teBefore, before)))) `sameAs` ((snd (ctx (teAfter, after))), NewNames)
+
+  xit "last expression is a case" $ do
+    let teBefore = create $
+          newVar "ax'" int64_t
+    let before =
+          [expr|
+              l2 <- eval l
+              case l2 of
+                (CNil) -> pure (CInt 0)
+                (CCons x xs) -> (CInt x') <- eval x
+                                (CInt s') <- sum xs
+                                ax' <- _prim_int_add x' s'
+                                pure (CInt ax')
+          |]
+    let teAfter = extend teBefore $
+          newVar "l2'" int64_t
+    let after =
+          [expr|
+              l2 <- eval l
+              do ccp.0 <- case l2 of
+                           (CNil) -> pure 0
+                           (CCons x xs) -> (CInt x') <- eval x
+                                           (CInt s') <- sum xs
+                                           ax' <- _prim_int_add x' s'
+                                           pure ax'
+              pure (CInt ccp.0)
+          |]
+    (caseCopyPropagation (snd (ctx (teBefore, before)))) `sameAs` ((snd (ctx (teAfter, after))), NewNames)
+
+runTests :: IO ()
+runTests = hspec spec
