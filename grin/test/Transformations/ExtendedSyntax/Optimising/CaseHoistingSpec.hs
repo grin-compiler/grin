6e093d690d62b191724301ceaac79edcc383c515
anabra <peter.d.podlovics@gmail.com>
2020-01-22 01:07:41 +0100

    ES: CaseHoisting tests

--- grin/test/Transformations/ExtendedSyntax/Optimising/CaseHoistingSpec.hs
+++ grin/test/Transformations/ExtendedSyntax/Optimising/CaseHoistingSpec.hs
@@ -1,14 +1,14 @@
 {-# LANGUAGE OverloadedStrings, QuasiQuotes, ViewPatterns #-}
 module Transformations.ExtendedSyntax.Optimising.CaseHoistingSpec where
 
-import Transformations.Optimising.CaseHoisting
-import Transformations.Names (ExpChanges(..))
+import Transformations.ExtendedSyntax.Optimising.CaseHoisting
 
 import Test.Hspec
-import Grin.TH
-import Test.Test hiding (newVar)
-import Test.Assertions
-import Grin.TypeCheck
+
+import Grin.ExtendedSyntax.TH
+import Grin.ExtendedSyntax.TypeCheck
+import Test.ExtendedSyntax.Assertions
+import Transformations.ExtendedSyntax.Names (ExpChanges(..))
 
 
 runTests :: IO ()
@@ -21,26 +21,26 @@ spec = do
           grinMain =
             v <- pure (CNil)
             u <- case v of
-              (CNil)        -> pure (CNil)
-              (CCons a1 b1) -> pure (CCons a1 b1)
+              (CNil)        @ alt1 -> pure (CNil)
+              (CCons a1 b1) @ alt2 -> pure (CCons a1 b1)
             case u of
-              (CNil)        -> pure 1
-              (CCons a2 b2) -> pure 2
+              (CNil)        @ alt3 -> pure alt3
+              (CCons a2 b2) @ alt4 -> pure (CNil)
       |]
     let after = [prog|
           grinMain =
             v <- pure (CNil)
             case v of
-              (CNil) ->
+              (CNil) @ alt1 ->
                 u.0 <- do
                   pure (CNil)
-                (CNil) <- pure u.0
-                pure 1
-              (CCons a1 b1) ->
+                alt3.0 <- pure u.0
+                pure alt3.0
+              (CCons a1 b1) @ alt2 ->
                 u.1 <- do
                   pure (CCons a1 b1)
-                (CCons a2.0 b2.0) <- pure u.1
-                pure 2
+                alt4.0 <- pure u.1
+                pure (CNil)
       |]
     caseHoisting (inferTypeEnv before) before `sameAs` (after, NewNames)
 
@@ -49,26 +49,26 @@ spec = do
           grinMain =
             v <- pure (CNil)
             u <- case v of
-              (CNil)        -> pure (CNil)
-              (CCons a1 b1) -> pure (CCons a1 b1)
+              (CNil)        @ alt1 -> pure (CNil)
+              (CCons a1 b1) @ alt2 -> pure (CCons a1 b1)
             r <- case u of
-              (CNil)        -> pure 1
-              (CCons a2 b2) -> pure 2
+              (CNil)        @ alt3 -> pure 1
+              (CCons a2 b2) @ alt4 -> pure 2
             pure r
       |]
     let after = [prog|
           grinMain =
             v <- pure (CNil)
             r <- case v of
-              (CNil) ->
+              (CNil) @ alt1 ->
                 u.0 <- do
                   pure (CNil)
-                (CNil) <- pure u.0
+                alt3.0 <- pure u.0
                 pure 1
-              (CCons a1 b1) ->
+              (CCons a1 b1) @ alt2 ->
                 u.1 <- do
                   pure (CCons a1 b1)
-                (CCons a2.0 b2.0) <- pure u.1
+                alt4.0 <- pure u.1
                 pure 2
             pure r
       |]
@@ -79,26 +79,27 @@ spec = do
           grinMain =
             v <- pure (CNil)
             u <- case v of
-              (CNil)        -> pure (CNil)
-              (CCons a1 b1) -> pure (CCons a1 b1)
+              (CNil)        @ alt1 -> pure (CNil)
+              (CCons a1 b1) @ alt2 -> pure (CCons a1 b1)
             r <- case u of
-              (CNil)        -> pure 1
-              #default      -> pure 2
+              (CNil)        @ alt3 -> pure (CNil)
+              #default      @ alt4 -> pure alt4
             pure r
       |]
     let after = [prog|
           grinMain =
             v <- pure (CNil)
             r <- case v of
-              (CNil) ->
+              (CNil) @ alt1 ->
                 u.0 <- do
                   pure (CNil)
-                (CNil) <- pure u.0
-                pure 1
-              (CCons a1 b1) ->
+                alt3.0 <- pure u.0
+                pure (CNil)
+              (CCons a1 b1) @ alt2 ->
                 u.1 <- do
                   pure (CCons a1 b1)
-                pure 2
+                alt4.0 <- pure u.1
+                pure alt4.0
             pure r
       |]
     caseHoisting (inferTypeEnv before) before `sameAs` (after, NewNames)
@@ -108,34 +109,41 @@ spec = do
           grinMain =
             v <- pure 1
             u <- case v of
-              0 -> pure (CNil)
-              1 -> pure (CCons 3 4)
+              0 @ alt1 -> pure (CNil)
+              1 @ alt2 -> pure (CCons v v)
             r <- case u of
-              (CNil)        -> pure (CEmpty)
-              #default      -> pure u
+              (CNil)   @ alt3 -> pure (CEmpty)
+              #default @ alt4 -> pure u
             q <- case r of
-              (CVoid)       -> pure (CEmpty)
-              #default      ->  _prim_int_print 777
-                                pure r
+              (CVoid) @ alt5 ->
+                pure (CEmpty)
+              #default @ alt6 ->
+                k0 <- pure 777
+                _1 <- _prim_int_print k0
+                pure r
             pure q
       |]
     let after = [prog|
           grinMain =
             v <- pure 1
             r <- case v of
-              0 ->
+              0 @ alt1 ->
                 u.0 <- do
                   pure (CNil)
-                (CNil) <- pure u.0
+                alt3.0 <- pure u.0
                 pure (CEmpty)
-              1 ->
+              1 @ alt2 ->
                 u.1 <- do
-                  pure (CCons 3 4)
+                  pure (CCons v v)
+                alt4.0 <- pure u.1
                 pure u.1
             q <- case r of
-              (CVoid)       -> pure (CEmpty)
-              #default      ->  _prim_int_print 777
-                                pure r
+              (CVoid) @ alt5 ->
+                pure (CEmpty)
+              #default @ alt6 ->
+                k0 <- pure 777
+                _1 <- _prim_int_print k0
+                pure r
             pure q
       |]
     caseHoisting (inferTypeEnv before) before `sameAs` (after, NewNames)
@@ -145,26 +153,27 @@ spec = do
           grinMain =
             v <- pure 1
             u <- case v of
-              0 -> pure (CNil)
-              1 -> pure (CCons 3 4)
+              0 @ alt1 -> pure (CNil)
+              1 @ alt2 -> pure (CCons v v)
             r <- case u of
-              #default  -> pure u
+              #default @ alt3 -> pure u
             q <- case r of
-              #default  -> pure r
+              #default @ alt4 -> pure r
             pure q
       |]
     let after = [prog|
           grinMain =
             v <- pure 1
             u <- case v of
-              0 ->
+              0 @ alt1 ->
                 pure (CNil)
-              1 ->
-                pure (CCons 3 4)
+              1 @ alt2 ->
+                pure (CCons v v)
             q <- case u of
-              #default ->
+              #default @ alt3 ->
                 r.0 <- do
                   pure u
+                alt4.0 <- pure r.0
                 pure r.0
             pure q
       |]
@@ -175,9 +184,9 @@ spec = do
           grinMain =
             v <- pure (CNil)
             u <- case v of
-              #default  -> pure v
+              #default @ alt1 -> pure v
             r <- case u of
-              #default  -> pure u
+              #default @ alt2 -> pure u
             x <- pure u
             pure r
       |]
@@ -185,9 +194,9 @@ spec = do
           grinMain =
             v <- pure (CNil)
             u <- case v of
-              #default  -> pure v
+              #default @ alt1 -> pure v
             r <- case u of
-              #default  -> pure u
+              #default @ alt2 -> pure u
             x <- pure u
             pure r
       |]
