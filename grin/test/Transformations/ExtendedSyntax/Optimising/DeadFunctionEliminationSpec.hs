d0c1d37e562a1d8d06662be002a29920b8b3674c
anabra <peter.d.podlovics@gmail.com>
2020-01-28 20:58:34 +0100

    ES: fixed tests for DFE

--- grin/test/Transformations/ExtendedSyntax/Optimising/DeadFunctionEliminationSpec.hs
+++ grin/test/Transformations/ExtendedSyntax/Optimising/DeadFunctionEliminationSpec.hs
@@ -1,48 +1,64 @@
 {-# LANGUAGE OverloadedStrings, QuasiQuotes #-}
 module Transformations.ExtendedSyntax.Optimising.DeadFunctionEliminationSpec where
 
+import Transformations.ExtendedSyntax.Optimising.DeadFunctionElimination (deadFunctionElimination)
+
+import Data.Either (fromRight)
+
 import Test.Hspec
-import Test.Hspec.PipelineExample
-import Pipeline.Pipeline hiding (pipeline)
-import Grin.TH
+
+import Test.ExtendedSyntax.Assertions
+import Grin.ExtendedSyntax.TH
+import Grin.ExtendedSyntax.Grin
+import Grin.ExtendedSyntax.PrimOpsPrelude (withPrimPrelude)
+import Grin.ExtendedSyntax.TypeCheck (inferTypeEnv)
+import AbstractInterpretation.ExtendedSyntax.LiveVariableSpec (calcLiveness)
 
 
 runTests :: IO ()
 runTests = hspec spec
 
+dfe :: Exp -> Exp
+dfe e = either error id $
+  deadFunctionElimination (calcLiveness e) (inferTypeEnv e) e
+
 spec :: Spec
 spec = do
   describe "Dead Function Elimination" $ do
 
-    let deadFunctionEliminationPipeline =
-          [ T DeadFunctionElimination
-          ]
-
     it "app_side_effect_1" $ do
       let before = [prog|
             grinMain =
-              p0 <- store (CInt 0)
+              k0 <- pure 0
+              n0 <- pure (CInt k0)
+              p0 <- store n0
               y0 <- f p0
               y1 <- fetch p0
               pure y1
 
             f p =
-              update p (CInt 1)
+              k1 <- pure 1
+              n1 <- pure (CInt k1)
+              _1 <- update p n1
               pure 0
           |]
 
       let after = [prog|
             grinMain =
-              p0 <- store (CInt 0)
+              k0 <- pure 0
+              n0 <- pure (CInt k0)
+              p0 <- store n0
               y0 <- f p0
               y1 <- fetch p0
               pure y1
 
             f p =
-              update p (CInt 1)
+              k1 <- pure 1
+              n1 <- pure (CInt k1)
+              _1 <- update p n1
               pure 0
           |]
-      pipelineSrc before after deadFunctionEliminationPipeline
+      dfe before `sameAs` after
 
     it "mutually_recursive" $ do
       let before = [prog|
@@ -54,30 +70,35 @@ spec = do
       let after = [prog|
             grinMain = pure 0
           |]
-      pipelineSrc before after deadFunctionEliminationPipeline
+      dfe before `sameAs` after
 
     it "replace_node" $ do
       let before = [prog|
             grinMain =
-              n0 <- f 0
+              k0 <- pure 0
+              n0 <- f k0
               pure 0
 
             f x =
-              p <- store (CInt 5)
+              k1 <- pure 1
+              n1 <- pure (CInt k1)
+              p <- store n1
               pure (CNode p)
           |]
 
       let after = [prog|
             grinMain =
+              k0 <- pure 0
               n0 <- pure (#undefined :: {CNode[#ptr]})
               pure 0
           |]
-      pipelineSrc before after deadFunctionEliminationPipeline
+      dfe before `sameAs` after
 
     it "replace_simple_type" $ do
       let before = [prog|
             grinMain =
-              y0 <- f 0
+              k0 <- pure 0
+              y0 <- f k0
               pure 0
 
             f x = pure x
@@ -85,10 +106,11 @@ spec = do
 
       let after = [prog|
             grinMain =
+              k0 <- pure 0
               y0 <- pure (#undefined :: T_Int64)
               pure 0
           |]
-      pipelineSrc before after deadFunctionEliminationPipeline
+      dfe before `sameAs` after
 
     it "simple" $ do
       let before = [prog|
@@ -100,24 +122,26 @@ spec = do
       let after = [prog|
             grinMain = pure 0
           |]
-      pipelineSrc before after deadFunctionEliminationPipeline
+      dfe before `sameAs` after
 
     it "true_side_effect_min" $ do
-      let before = [prog|
+      let before = withPrimPrelude [prog|
             grinMain =
               result_main <- Main.main1 $
               pure ()
 
             Main.main1 =
-              _prim_int_print $ 1
+              k0 <- pure 0
+              _prim_int_print $ k0
           |]
 
-      let after = [prog|
+      let after = withPrimPrelude [prog|
             grinMain =
               result_main <- Main.main1 $
               pure ()
 
             Main.main1 =
-              _prim_int_print $ 1
+              k0 <- pure 0
+              _prim_int_print $ k0
           |]
-      pipelineSrc before after deadFunctionEliminationPipeline
+      dfe before `sameAs` after
