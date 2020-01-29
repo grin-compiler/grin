c310dead099946dcb59cb1d5f0756bd7b7b73eeb
anabra <peter.d.podlovics@gmail.com>
2020-01-29 12:36:09 +0100

    ES: fixed DPE tests

--- grin/test/Transformations/ExtendedSyntax/Optimising/DeadParameterEliminationSpec.hs
+++ grin/test/Transformations/ExtendedSyntax/Optimising/DeadParameterEliminationSpec.hs
@@ -7,6 +7,7 @@ import Data.Either
 
 import Test.Hspec
 
+import Test.ExtendedSyntax.Util (loadTestData)
 import Test.ExtendedSyntax.Assertions
 import Grin.ExtendedSyntax.TH
 import Grin.ExtendedSyntax.Grin
@@ -29,7 +30,8 @@ spec = do
     it "Fnode" $ do
       let before = [prog|
             grinMain =
-              x0 <- pure (CInt 5)
+              k0 <- pure 5
+              x0 <- pure (CInt k0)
               p0 <- store x0
               a0 <- pure (Ffoo p0 p0 p0)
               p1 <- store a0
@@ -44,16 +46,17 @@ spec = do
             eval p =
               v <- fetch p
               case v of
-                (CInt n) -> pure v
-                (Ffoo x1 y1 z1) ->
+                (CInt n) @ alt1 -> pure v
+                (Ffoo x1 y1 z1) @ alt2 ->
                   w <- foo x1 y1 z1
-                  update p w
+                  _1 <- update p w
                   pure w
           |]
 
       let after = [prog|
             grinMain =
-              x0 <- pure (CInt 5)
+              k0 <- pure 5
+              x0 <- pure (CInt k0)
               p0 <- store x0
               a0 <- pure (Ffoo p0 p0 p0)
               p1 <- store a0
@@ -70,10 +73,10 @@ spec = do
             eval p =
               v <- fetch p
               case v of
-                (CInt n) -> pure v
-                (Ffoo x1 y1 z1) ->
+                (CInt n) @ alt1 -> pure v
+                (Ffoo x1 y1 z1) @ alt2 ->
                   w <- foo y1
-                  update p w
+                  _1 <- update p w
                   pure w
           |]
       dpe before `sameAs` after
@@ -84,75 +87,89 @@ spec = do
     --   "dead-parameter-elimination/pnode_after.grin"
     --   deadParameterEliminationPipeline
 
+
+    it "PNode" $ do
+      before <- loadTestData "dead-parameter-elimination/pnode_before.grin"
+      after  <- loadTestData "dead-parameter-elimination/pnode_after.grin"
+      dpe before `sameAs` after
+
     it "Pnode opt" $ do
       let before = [prog|
             grinMain =
-              a0 <- pure (CInt 5)
-              a1 <- pure (CInt 5)
-              a2 <- pure (CInt 5)
+              k0 <- pure 5
+              a0 <- pure (CInt k0)
+              a1 <- pure (CInt k0)
+              a2 <- pure (CInt k0)
               p0 <- store a0
               p1 <- store a1
               p2 <- store a2
 
-              foo3   <- pure (P3foo)
+              foo3 <- pure (P3foo)
 
-              (P3foo) <- pure foo3
-              foo2    <- pure (P2foo p0)
+              (P3foo) @ _1 <- pure foo3
+              foo2 <- pure (P2foo p0)
 
-              (P2foo v0) <- pure foo2
-              foo1       <- pure (P1foo v0 p1)
+              (P2foo v0) @ _2 <- pure foo2
+              foo1 <- pure (P1foo v0 p1)
 
-              (P1foo v1 v2) <- pure foo1
-              fooRet        <- foo v1 v2 p2
+              (P1foo v1 v2) @ _3 <- pure foo1
+              fooRet <- foo v1 v2 p2
               pure fooRet
 
             foo x0 y0 z0 =
               y0' <- fetch y0
-              (CInt n) <- y0'
+              (CInt n) @ _4 <- y0'
               pure y0'
           |]
 
       let after = [prog|
             grinMain =
-              a0 <- pure (CInt 5)
-              a1 <- pure (CInt 5)
-              a2 <- pure (CInt 5)
+              k0 <- pure 5
+              a0 <- pure (CInt k0)
+              a1 <- pure (CInt k0)
+              a2 <- pure (CInt k0)
               p0 <- store a0
               p1 <- store a1
               p2 <- store a2
 
-              foo3   <- pure (P3foo)
+              foo3 <- pure (P3foo)
 
-              (P3foo) <- pure foo3
-              foo2    <- pure (P2foo p0)
+              (P3foo) @ _1 <- pure foo3
+              foo2 <- pure (P2foo p0)
 
-              (P2foo v0) <- pure foo2
-              foo1       <- pure (P1foo v0 p1)
+              (P2foo v0) @ _2 <- pure foo2
+              foo1 <- pure (P1foo v0 p1)
 
-              (P1foo v1 v2) <- pure foo1
-              fooRet        <- foo v2
+              (P1foo v1 v2) @ _3 <- pure foo1
+              fooRet <- foo v2
               pure fooRet
 
             foo y0 =
               z0 <- pure (#undefined :: #ptr)
               x0 <- pure (#undefined :: #ptr)
               y0' <- fetch y0
-              (CInt n) <- y0'
+              (CInt n) @ _4 <- y0'
               pure y0'
           |]
       dpe before `sameAs` after
 
     it "Simple" $ do
       let before = [prog|
-            grinMain = g 5
+            grinMain =
+              k0 <- pure 5
+              g k0
 
             f x y = pure x
 
-            g z = f 0 z
+            g z =
+              k1 <- pure 0
+              f k1 z
           |]
 
       let after = [prog|
-            grinMain = g
+            grinMain =
+              k0 <- pure 5
+              g
 
             f x =
               y <- pure (#undefined :: T_Int64)
@@ -160,30 +177,41 @@ spec = do
 
             g =
               z <- pure (#undefined :: T_Int64)
-              f 0
+              k1 <- pure 0
+              f k1
           |]
       dpe before `sameAs` after
 
     it "Mutually recursive" $ do
       let before = [prog|
-            grinMain = f 0 0
+            grinMain =
+              k0 <- pure 0
+              f k0 k0
 
-            f x y = g x 0
+            f x y =
+              k1 <- pure 0
+              g x k1
 
-            g v w = f 0 w
+            g v w =
+              k2 <- pure 0
+              f k2 w
           |]
 
       let after = [prog|
-            grinMain = f
+            grinMain =
+              k0 <- pure 0
+              f
 
             f =
               y <- pure (#undefined :: T_Int64)
               x <- pure (#undefined :: T_Int64)
+              k1 <- pure 0
               g
 
             g =
               w <- pure (#undefined :: T_Int64)
               v <- pure (#undefined :: T_Int64)
+              k2 <- pure 0
               f
           |]
       dpe before `sameAs` after
