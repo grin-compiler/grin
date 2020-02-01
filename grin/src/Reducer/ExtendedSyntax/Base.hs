e2bb2e7d283d8d36ab30bfd26bc6464d2dfc8220
anabra <peter.d.podlovics@gmail.com>
2020-02-01 23:55:56 +0100

    ES: added Reducer/Base

---
+++ grin/src/Reducer/ExtendedSyntax/Base.hs
@@ -0,0 +1,87 @@
+{-# LANGUAGE LambdaCase, TupleSections, BangPatterns #-}
+module Reducer.ExtendedSyntax.Base where
+
+import Data.Map (Map)
+import qualified Data.Map as Map
+
+import Text.PrettyPrint.ANSI.Leijen
+
+import Grin.Grin
+import Grin.Pretty
+
+-- models cpu registers
+type Env = Map Name RTVal
+
+type SimpleRTVal = RTVal
+data RTVal
+  = RT_ConstTagNode Tag  [SimpleRTVal]
+  | RT_VarTagNode   Name [SimpleRTVal]
+  | RT_ValTag       Tag
+  | RT_Unit
+  | RT_Lit          Lit
+  | RT_Var          Name
+  | RT_Loc          Int
+  | RT_Undefined
+  deriving (Show, Eq, Ord)
+
+
+instance Pretty RTVal where
+  pretty = \case
+    RT_ConstTagNode tag args -> parens $ hsep (pretty tag : map pretty args)
+    RT_VarTagNode name args  -> parens $ hsep (pretty name : map pretty args)
+    RT_ValTag tag -> pretty tag
+    RT_Unit       -> parens empty
+    RT_Lit lit    -> pretty lit
+    RT_Var name   -> pretty name
+    RT_Loc a      -> keyword "loc" <+> int a
+    RT_Undefined  -> keyword "undefined"
+
+keyword :: String -> Doc
+keyword = yellow . text
+
+selectNodeItem :: Maybe Int -> RTVal -> RTVal
+selectNodeItem Nothing val = val
+selectNodeItem (Just 0) (RT_ConstTagNode tag args) = RT_ValTag tag
+selectNodeItem (Just i) (RT_ConstTagNode tag args) = args !! (i - 1)
+
+bindPatMany :: Env -> [RTVal] -> [LPat] -> Env
+bindPatMany env [] [] = env
+bindPatMany env (val : vals) (lpat : lpats) = bindPatMany (bindPat env val lpat) vals lpats
+bindPatMany env [] (lpat : lpats) = bindPatMany (bindPat env RT_Undefined lpat) [] lpats
+bindPatMany _ vals lpats = error $ "bindPatMany - pattern mismatch: " ++ show (vals, lpats)
+
+bindPat :: Env -> RTVal -> LPat -> Env
+bindPat env !val lpat = case lpat of
+  Var n -> case val of
+              RT_ValTag{}   -> Map.insert n val env
+              RT_Unit       -> Map.insert n val env
+              RT_Lit{}      -> Map.insert n val env
+              RT_Loc{}      -> Map.insert n val env
+              RT_Undefined  -> Map.insert n val env
+              _ -> Map.insert n val env -- WTF????
+              _ -> error $ "bindPat - illegal value: " ++ show val
+  ConstTagNode ptag pargs -> case val of
+                  RT_ConstTagNode vtag vargs | ptag == vtag -> bindPatMany env vargs pargs
+                  _ -> error $ "bindPat - illegal value for ConstTagNode: " ++ show val ++ " vs " ++ show (ConstTagNode ptag pargs)
+  VarTagNode varname pargs -> case val of
+                  RT_ConstTagNode vtag vargs -> bindPatMany (Map.insert varname (RT_ValTag vtag) env) vargs pargs
+                  _ -> error $ "bindPat - illegal value for ConstTagNode: " ++ show val
+  Unit -> env
+  _ -> error $ "bindPat - pattern mismatch" ++ show (val,lpat)
+
+lookupEnv :: Name -> Env -> RTVal
+lookupEnv n env = Map.findWithDefault (error $ "missing variable: " ++ unpackName n) n env
+
+evalVal :: Env -> Val -> RTVal
+evalVal env = \case
+  Lit lit     -> RT_Lit lit
+  Var n       -> lookupEnv n env
+  ConstTagNode t a -> RT_ConstTagNode t $ map (evalVal env) a
+  VarTagNode n a -> case lookupEnv n env of
+                  RT_Var n     -> RT_VarTagNode n $ map (evalVal env) a
+                  RT_ValTag t  -> RT_ConstTagNode t $ map (evalVal env) a
+                  x -> error $ "evalVal - invalid VarTagNode tag: " ++ show x
+  ValTag tag  -> RT_ValTag tag
+  Unit        -> RT_Unit
+  Undefined t -> RT_Undefined
+  x -> error $ "evalVal: " ++ show x
