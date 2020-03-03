e7032e8b519971bfb6ea115e034f1c8dcd48c5c7
anabra <peter.d.podlovics@gmail.com>
2020-03-03 22:29:32 +0100

    ES: updated DFE

--- grin/src/Transformations/ExtendedSyntax/Optimising/DeadFunctionElimination.hs
+++ grin/src/Transformations/ExtendedSyntax/Optimising/DeadFunctionElimination.hs
@@ -15,22 +15,36 @@ import Text.Printf
 import Grin.ExtendedSyntax.Grin
 
 deadFunctionElimination :: Program -> Program
-deadFunctionElimination exp@(Program exts defs) = Program exts [def | def@(Def name _ _) <- defs, Set.member name liveDefs] where
+deadFunctionElimination exp@(Program exts defs) = Program liveExts liveDefs where
+  liveExts :: [External]
+  liveExts = [ext | ext <- exts, Set.member (eName ext) liveNames]
+
+  liveDefs :: [Exp]
+  liveDefs = [def | def@(Def name _ _) <- defs, Set.member name liveSet]
+
+  liveNames :: Set Name
+  liveNames = cata collectAll $ Program [] liveDefs -- collect all live names
+
   defMap :: Map Name Def
   defMap = Map.fromList [(name, def) | def@(Def name _ _) <- defs]
 
   lookupDef :: Name -> Maybe Def
   lookupDef name = Map.lookup name defMap
 
-  liveDefs :: Set Name
-  liveDefs = fst $ until (\(live, visited) -> live == visited) visit (Set.singleton "grinMain", mempty)
+  liveSet :: Set Name
+  liveSet = fst $ until (\(live, visited) -> live == visited) visit (Set.singleton "grinMain", mempty)
 
   visit :: (Set Name, Set Name) -> (Set Name, Set Name)
   visit (live, visited) = (mappend live seen, mappend visited toVisit) where
     toVisit = Set.difference live visited
-    seen    = foldMap (maybe mempty (cata collect) . lookupDef) toVisit
+    seen    = foldMap (maybe mempty (cata collectFuns) . lookupDef) toVisit
 
-  collect :: ExpF (Set Name) -> Set Name
-  collect = \case
-    SAppF name _ | not (isExternalName exts name) -> Set.singleton name
+  collectFuns :: ExpF (Set Name) -> Set Name
+  collectFuns = \case
+    SAppF name _ | Map.member name defMap -> Set.singleton name
+    exp -> Data.Foldable.fold exp
+
+  collectAll :: ExpF (Set Name) -> Set Name
+  collectAll = \case
+    SAppF name args -> Set.singleton name
     exp -> Data.Foldable.fold exp
